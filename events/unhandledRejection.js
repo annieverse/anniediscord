36e0d918d4875602abeef9d378a49a61d9b58eec
unknown <nightcore.raven07@gmail.com>
2019-05-28 23:15:32 +0700

    fix unhandledrejection variable

--- events/unhandledRejection.js
+++ events/unhandledRejection.js
@@ -35,7 +35,7 @@ module.exports = (bot, err, p, message) => {
         .addField("User", metadata.user.mention,true)
         .addField("Channel", metadata.channel,true)
         .setDescription(`
-        "${metadata.error}"
+        "${metadata.user.msg}"
         \`\`\`javascript\n${y}\`\`\`
         `)
         .setTimestamp(Date.now())
