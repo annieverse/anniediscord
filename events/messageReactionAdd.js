7189365b943d992350fa51acb162b3bae11e7cf4
unknown <nightcore.raven07@gmail.com>
2019-05-29 00:38:14 +0700

    feature system fix & saved pm2

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,46 +1,35 @@
 const Discord = require("discord.js");
+const palette = require(`../colorset.json`);
 const sql = require("sqlite");
 sql.open(".data/database.sqlite");
 
 module.exports = async (bot, reaction, user) => {
 
-  //  I changed the structure for readability. -naphnaphz.
-
-
-  //  Centralized Object.
-  let metadata = {
-      art_channels: [
-        "459892609838481408",
-        "459893040753016872",
-        "460439050445258752",
-        "461926519976230922",
-        "460615254553001994",
-        "538806382779170826",
-        "565308091424571422"
-      ],
-      featured_channel: bot.channels.get("581642059090362368"),
-  }
+  let favoritechannel = bot.channels.get("581642059090362368"); // channel the image is sent to
 
   const rmsg = reaction.message;
   const member = await rmsg.guild.fetchMember(user);
   
+  if(user.bot)return;
 
-  //  Core processes
-  const main = async () => {
-    let filtered_reaction = rmsg.reactions.filter(reaction => reaction.emoji.name == "⭐").first();
+  const artChannels = [
+    "459892609838481408",
+    "459893040753016872",
+    "460439050445258752",
+    "461926519976230922",
+    "460615254553001994",
+    "538806382779170826",
+    "565308091424571422"];
 
+  if (reaction.emoji.name == "⭐" && artChannels.includes(rmsg.channel.id)) { // change rmsg.channel.id == "530223957534703636" for the art channels
+    let x = rmsg.reactions.filter(reaction => reaction.emoji.name == "⭐").first();
+    if (rmsg.author.id =='514688969355821077')return;//make sure its not bots id
 
-    //  Returns preview url if the user shared post from social media.
-    //  Else, get attachment's url.
-    const get_attachment = () => {
-      return rmsg.attachments.first().url ?  rmsg.attachments.first().url : rmsg.embeds.proxyURL;
-    }
+    
 
-    console.log(`hoi its here!` + filtered_reaction);
-
-    if(filtered_reaction.count === 1){
-
-      let attachment = get_attachment();
+    if(x.count===1){
+      // Do Code Here
+      let attachment = rmsg.attachments.first().url;
 
       //let fileSize = rmsg.attachments.first().filesize;
       //let fileSizelimit = 8000000;
@@ -49,7 +38,6 @@ module.exports = async (bot, reaction, user) => {
         .setImage(attachment)
         .setAuthor(rmsg.author.tag, rmsg.author.avatarURL)
         .setColor(palette.darkmatte)
-        .setTimestamp()
         .setFooter(rmsg.id);
 
         /*  //Pan Version: 4 Messages (ID / Embed / LB / LB)
@@ -59,31 +47,9 @@ module.exports = async (bot, reaction, user) => {
          */
 
         //Fwubbles Version: 1 Embed Message (ID in footer)
-        metadata.featured_channel.send(embed);
-        //metadata.featured_channel.send(`${rmsg.author}`).then(msg=>msg.delete());      
+        favoritechannel.send(embed);
+        favoritechannel.send(`${rmsg.author}`).then(msg=>msg.delete());      
     }
-
   }
-
-
-  //  Initialize
-  const run = async () => {
-
-    //  Returns if user is bot.
-    if(user.bot)return;
-
-
-    //  Make sure its not bots id
-    if(rmsg.author.id =='514688969355821077')return;
-
-
-    //  Returns if the react is not a star or the channel is not listed in arts channels.
-    //  change rmsg.channel.id == "530223957534703636" for the art channels
-    if(reaction.emoji.name !== "⭐" && !metadata.art_channels.includes(rmsg.channel.id))return;
-
-    main();
-
-  }
-
-  run();
+  
 }
