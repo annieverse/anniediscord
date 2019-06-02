5f048586a7db2fb9674589efe74ea46c0486a36a
unknown <nightcore.raven07@gmail.com>
2019-06-03 03:54:49 +0700

    experienceMessage.js refactor

--- utils/eventHandler.js
+++ utils/eventHandler.js
@@ -14,14 +14,14 @@ module.exports = bot => {
     //process.on('unhandledRejection', (err, p) => reqEvent("unhandledRejection")(bot,err,p,message_object));
     //
 
-    //if (env.dev) {
+    if (!env.dev) {
         bot.on("guildMemberAdd", async(member) => reqEvent("guildMemberAdd")(bot, member));
         bot.on("guildMemberRemove", async(member) => reqEvent("guildMemberRemove")(bot, member));
         bot.on("guildMemberUpdate", async(oldUser, newUser) => reqEvent("guildMemberUpdate")(bot, oldUser, newUser));    
         bot.on("messageReactionAdd", async (reaction, user) => reqEvent("messageReactionAdd")(bot, reaction, user));
         bot.on("messageReactionRemove", async (reaction, user) => reqEvent("messageReactionRemove")(bot, reaction, user));
         bot.on("raw", async (packet) => reqEvent("raw")(bot, packet));
-    //}
+    }
 
     if (env.active_exp) bot.on("message", async(message) => reqEvent("experienceMessage")(bot, message));
 }
