46e4a535e18174b503fde8fce36e57cb2ce477b8
unknown <nightcore.raven07@gmail.com>
2019-06-05 19:26:12 +0700

    removed few colors from ruling star

--- events/ready.js
+++ events/ready.js
@@ -49,10 +49,7 @@ module.exports = bot => {
             'b5EAD7',
             'C7CEEA',
             'F8B195',
-            'F67280',
-            'C06C84',
-            '6C5B7B',
-            '355C7D'
+            'F67280'
         ]
 
         /**
