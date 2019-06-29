44fcef6e34f81e1ccde80dc8100f2db4aa6ee608
unknown <nightcore.raven07@gmail.com>
2019-06-29 17:08:45 +0700

    Store token into dotenv, Simplified startup modules

--- server.js
+++ server.js
@@ -1,72 +1,29 @@
+//  Initialize startup modules
 console.time(`Initialized In`);
-
-const Discord = require("discord.js");
-const env = require('./.data/environment.json');
-const fs = require("fs");
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
-require("./utils/eventHandler")(bot)
+const { Client } = require("discord.js");
+let bot = new Client();
+const modulesLoader = require(`./utils/modulesLoader`);
+const consoleChat = require(`./utils/consoleChat`);
 const express = require('express');
 const app = express();
 
-
 //	Ping server so it won't died cause of idling.
-app.get("/", (request, response) => {
-    console.log(`${bot.ping}ms ping was received`);
-    response.sendStatus(200);
-});
-
+app.get("/", (request, response) => response.sendStatus(200))
 
 //  To prevent PM2 from being terminated.
-const listener = app.listen(process.env.PORT, function() {
-    console.log('Your app is listening on port ' + listener.address().port);
-});
-
+const listener = app.listen(process.env.PORT, () => console.log(`Port ${listener.address().port} OK`))
 
 //	Loading command modules.
-fs.readdir("./modules/commands/", (err, files) => {
+bot = new modulesLoader().register(bot)
 
-    if (err) console.log(err);
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if (jsfile.length <= 0) {
-        console.log(`missing files`);
-        return;
-    }
+//  Loads .env variables
+require(`dotenv`).config()
 
-    jsfile.forEach((f) => {
-        let props = require(`./modules/commands/${f}`);
-        bot.commands.set(props.help.name, props);
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.name)
-        });
-    });
-    console.log(`${jsfile.length} command files have been loaded.`)
-});
-
+//  Start events.
+require("./utils/eventHandler")(bot)
 
 //  Bot Messaging via Console ♡
-let y = process.openStdin()
-y.addListener("data", res => {
-    
+consoleChat(bot)
 
-
-    let x = res.toString().trim().split(/ +/g);
-    let msg = x.join(" ")
-
-    let channel = "sandbox"
-    //let channel = "general"
-    //let channel = "general-2"
-    //let channel = "vc-off-topic"
-    //let channel = "staff-hq"
-    let enabletextwrapping = true;
-    //let enabletextwrapping = false;
-
-    bot.channels.get(bot.channels.find(x => x.name === channel).id).send(msg);
-});
-
-
-//	Client token.
-const token = process.env.TOKEN ? process.env.TOKEN : env.temp_token;
-console.log(env.dev ? `Local development server has been started.` : `Production server has been started.`)
-bot.login(token)
+//	Login.
+bot.login(process.env.TOKEN)
---
+++ utils/modulesLoader.js
@@ -0,0 +1,34 @@
+console.time(`Commands Loaded`)
+const { readdir } = require(`fs`);
+const { Collection } = require(`discord.js`);
+
+class modulesLoader {
+    //	Loading command modules.
+    register(Client) {
+        Client.commands = new Collection();
+        Client.aliases = new Collection();
+
+        readdir("./modules/commands/", (err, files) => {
+
+            if (err) console.log(err);
+            let jsfile = files.filter(f => f.split(".").pop() === "js")
+            if (jsfile.length <= 0) {
+                console.log(`missing files`);
+                return;
+            }
+
+            jsfile.forEach((f) => {
+                let props = require(`../modules/commands/${f}`);
+                Client.commands.set(props.help.name, props);
+                props.help.aliases.forEach(alias => {
+                    Client.aliases.set(alias, props.help.name)
+                });
+            });
+        });
+
+        console.timeEnd(`Commands Loaded`)
+        return Client;
+    }
+}
+
+module.exports = modulesLoader;
