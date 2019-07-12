e7429972a4e0354d4b976f3b86395b9590ffae1f
BaitGod01 <Baitgod01@gmail.com>
2019-07-12 14:02:38 -0400

    event role removed when given the muted role

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -5,8 +5,18 @@ module.exports = (bot, oldUser, newUser) => {
   }
   
   let ticket = getRoles('Nickname Changer');
+  let muted = getRoles('muted');
+  let eventParticipant = getRoles('Event Participant');
+
   if( newUser.roles.has(ticket.id) && (oldUser.nickname !== newUser.nickname) ) {
     console.log(`${newUser.nickname} used the nickname changer ticket.`)
     newUser.removeRole(ticket);
   }
+  if (newUser.roles.has(muted.id)){
+    if(newUser.roles.has(eventParticipant.id)){
+      newUser.removeRole(eventParticipant.id)
+      console.log(`${newUser.nickname} was given the ${muted.name} role so their event participant role has been taken away.`)
+    }
+  }
+  
 }
