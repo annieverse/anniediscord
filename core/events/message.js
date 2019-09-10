fbfedd88f37bcc5952027b8eb5d3ff6c3d874ddb
Sunny-Rainy-Works <ametotaiyounoe@gmail.com>
2019-09-10 21:36:18 +0200

    lock message.js because I think the glitch is because of a race condition

--- core/events/message.js
+++ core/events/message.js
@@ -1,46 +1,48 @@
 const Worker = require(`../utils/Worker`)
 const Pistachio = require(`../utils/Pistachio`)
+var AsyncLock = require(`async-lock`)
 
 module.exports = async (bot, message) => {
+	var lock = new AsyncLock()
+	var data
+	lock.acquire(bot.db, async function() {//async work
+		//	Set current author of the message
+		bot.db.setUser = message.author.id
 
-	//	Set current author of the message
-	bot.db.setUser = message.author.id
+		//	Register new data if its a new user, else ignore.
+		await bot.db.validatingNewUser()
 
-	//	Register new data if its a new user, else ignore.
-	await bot.db.validatingNewUser()
+		//	Retrieve user metadata from db
+		data = await bot.db.userMetadata
+	}, function() {// lock released
+		/**
+		 * This used to supply the required metadata in Message Controller.
+		 * Also remember, this is only used outside of command-environment.
+		 */
+		let stacks = new Pistachio({
+			label: `msg-${message.author.id}`,
+			applyTicketBuffs: true,
+			applyCardBuffs: true,
+			bonus: 0,
+			gainArtcoins: true,
+			meta: {
+				author: message.author,
+				data: data
+			},
+			bot: bot,
+			message: message,
+			cooldown: 30000,
+			total_gained_exp: Math.round(Math.random() * (15 - 10 + 1)) + 10,
+			total_gained_ac: Math.round(Math.random() * (15 - 10 + 1)) + 10,
+			updated: {
+				currentexp: 0,
+				level: 0,
+				maxexp: 0,
+				nextexpcurve: 0
+			}
+		}).bag()
 
-	//	Retrieve user metadata from db
-	const data = await bot.db.userMetadata
-
-	/**
-	 * This used to supply the required metadata in Message Controller.
-	 * Also remember, this is only used outside of command-environment.
-	 */
-	let stacks = new Pistachio({
-		label: `msg-${message.author.id}`,
-		applyTicketBuffs: true,
-		applyCardBuffs: true,
-		bonus: 0,
-		gainArtcoins: true,
-		meta: {
-			author: message.author,
-			data: data
-		},
-		bot: bot,
-		message: message,
-		cooldown: 30000,
-		total_gained_exp: Math.round(Math.random() * (15 - 10 + 1)) + 10,
-		total_gained_ac: Math.round(Math.random() * (15 - 10 + 1)) + 10,
-		updated: {
-			currentexp: 0,
-			level: 0,
-			maxexp: 0,
-			nextexpcurve: 0
-		}
-	}).bag()
-
-
-	//	Check for message flow
-	new Worker(stacks).default()
-
+		//	Check for message flow
+		new Worker(stacks).default()
+	})
 }
