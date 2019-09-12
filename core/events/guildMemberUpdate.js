0c0f93337cf753437bf617c082fdf51f31c58886
secretdwarf05 <gwilliams314965@gmail.com>
2019-09-12 17:37:41 -0400

    changed guildmemberupdate and eventhandler a bit

--- core/events/guildMemberUpdate.js
+++ core/events/guildMemberUpdate.js
@@ -6,11 +6,21 @@ module.exports = (bot, oldUser, newUser) => {
 	function getRoles(r) {
 		return bot.guilds.get(`459891664182312980`).roles.find(n => n.name === r)
 	}
-  
+	function getRolesById(r) {
+		return bot.guilds.get(`459891664182312980`).roles.find(n => n.id === r)
+	}
 	let ticket = getRoles(`Nickname Changer`)
 	let muted = getRoles(`muted`)
 	let eventParticipant = getRoles(`Event Participant`)
+	let apprenticeship = getRolesById(`621729846753755136`)
+	let classRoomChannel = bot.channels.get(`565308091424571422`)
 
+	console.log(apprenticeship)
+
+	if (newUser.roles.has(apprenticeship.id)) {
+		logger.info(`${newUser.nickname} joined the classrooms.`)
+		classRoomChannel.send(`${newUser.nickname} joined the classrooms.`)
+	}
 	if( newUser.roles.has(ticket.id) && (oldUser.nickname !== newUser.nickname) ) {
 		logger.info(`${newUser.nickname} used the nickname changer ticket.`)
 		newUser.removeRole(ticket)
--- core/utils/eventHandler.js
+++ core/utils/eventHandler.js
@@ -13,7 +13,7 @@ module.exports = bot => {
 		message_object = message
 		reqEvent(`message`)(bot, message)
 	})
-	
+	bot.on(`guildMemberUpdate`, async(oldUser, newUser) => reqEvent(`guildMemberUpdate`)(bot, oldUser, newUser))
 
 
 	/**
@@ -28,7 +28,7 @@ module.exports = bot => {
 		bot.on(`disconnect`, (bot) => reqEvent(`disconnect`)(bot))
 		bot.on(`guildMemberAdd`, async(member) => reqEvent(`guildMemberAdd`)(bot, member))
 		bot.on(`guildMemberRemove`, async(member) => reqEvent(`guildMemberRemove`)(bot, member))
-		bot.on(`guildMemberUpdate`, async(oldUser, newUser) => reqEvent(`guildMemberUpdate`)(bot, oldUser, newUser))    
+	/** bot.on(`guildMemberUpdate`, async(oldUser, newUser) => reqEvent(`guildMemberUpdate`)(bot, oldUser, newUser))*/	    
 		bot.on(`raw`, async (packet) => reqEvent(`raw`)(bot, packet))
 		bot.on(`messageReactionAdd`, async (reaction, user) => reqEvent(`messageReactionAdd`)({bot, reaction, user, message_object}))
 	}
