53fdf0057d742a9e0713a6f19c79ffc914af8133
secretdwarf05 <gwilliams314965@gmail.com>
2019-09-12 17:54:05 -0400

    changed lines 22 and 21 along with line 9 (notread

--- core/events/guildMemberUpdate.js
+++ core/events/guildMemberUpdate.js
@@ -12,14 +12,14 @@ module.exports = (bot, oldUser, newUser) => {
 	let ticket = getRoles(`Nickname Changer`)
 	let muted = getRoles(`muted`)
 	let eventParticipant = getRoles(`Event Participant`)
-	let apprenticeship = getRolesById(`621729846753755136`)
+	let apprenticeship = getRoles(`Apprenticeship`)
 	let classRoomChannel = bot.channels.get(`565308091424571422`)
 
-	console.log(apprenticeship)
+	
 
 	if (newUser.roles.has(apprenticeship.id)) {
-		logger.info(`${newUser.nickname} joined the classrooms.`)
-		classRoomChannel.send(`${newUser.nickname} joined the classrooms.`)
+		logger.info(`${newUser.user.username} joined the classroom.`)
+		classRoomChannel.send(`${newUser} joined the classroom.`)
 	}
 	if( newUser.roles.has(ticket.id) && (oldUser.nickname !== newUser.nickname) ) {
 		logger.info(`${newUser.nickname} used the nickname changer ticket.`)
