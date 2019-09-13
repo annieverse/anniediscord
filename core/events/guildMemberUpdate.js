0babcbe285742ab8a1410fd6de8f56d25fd2c0aa
secretdwarf05 <gwilliams314965@gmail.com>
2019-09-13 12:40:55 -0400

    changed channel id

--- core/events/guildMemberUpdate.js
+++ core/events/guildMemberUpdate.js
@@ -12,7 +12,7 @@ module.exports = (bot, oldUser, newUser) => {
 	let eventParticipant = getRoles(`Event Participant`)
 	let apprenticeship = getRoles(`Apprenticeship`)
 
-	let classRoomChannel = bot.channels.get(`622066583002677270`)
+	let classRoomChannel = bot.channels.get(`621705949429891074`)
 
 	if (newUser.roles.has(apprenticeship.id) && !oldUser.roles.has(apprenticeship.id)) {
 		logger.info(`${newUser.user.username} joined the classroom.`)
