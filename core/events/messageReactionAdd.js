4933cad8b70d4af8d810d6108dcb70a1f8998233
Naph <nightcore.raven07@gmail.com>
2019-09-20 01:20:52 +0700

    passed local test

--- core/events/messageReactionAdd.js
+++ core/events/messageReactionAdd.js
@@ -14,22 +14,8 @@ module.exports = async(Components) => {
 
         const { bot, reaction, user } = Components
         let classRoomChannel = bot.channels.get(`621705949429891074`)
-        const rmsg = reaction.message;
-        const member = await rmsg.guild.fetchMember(user);
-
-        function getRoles(r) {
-            const guild = bot.guilds.get(`459891664182312980`);
-            const role = guild.roles.find(n => n.id === r);
-            return role;
-        }
-
-        function getEmoji(e) {
-            if (typeof e == "string") {
-                return bot.emojis.find(emoji => emoji.name === e);
-            } else {
-                return e;
-            }
-        }
+        const rmsg = reaction.message
+        const member = await rmsg.guild.fetchMember(user)
 
         const ApprenticeKey = `621714766947287052`
         const Announcements = `614737097454125056`
@@ -45,7 +31,7 @@ module.exports = async(Components) => {
 
         //    Assign key, send notification.
     	bot.logger.info(`${rmsg.author.tag} has received Apprenticeship Key.`)
-        await member.addRole(ApprenticeKey);
+        await member.addRole(ApprenticeKey)
         return classRoomChannel.send(`${rmsg.author} has joined the classroom!`)
     }
 }
