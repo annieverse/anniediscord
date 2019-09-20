a762df318f2c7d5d240ac55f9dd4ac97dae930bb
Naph <nightcore.raven07@gmail.com>
2019-09-20 21:05:48 +0700

    added classroomManager

---
+++ core/utils/classroomManager.js
@@ -0,0 +1,28 @@
+module.exports = async (Components) => {
+	 /**
+     *
+     *	This function can be wrapped into its own class.
+     *	@classRoom
+     */
+        const { bot, reaction, user, rmsg } = Components
+        let classRoomChannel = bot.channels.get(`621705949429891074`)
+        const rmsg = reaction.message
+        const member = await rmsg.guild.fetchMember(user)
+
+        const ApprenticeKey = `621714766947287052`
+        const Announcements = `459892157600366612`
+
+        //    Ignore message outside of #announcements channel.
+        if (rmsg.channel.id != Announcements) return
+
+        //    Ignore message if reaction is not a pencil.    
+        if (reaction.emoji.name != `✏`) return
+
+        //    Ignore if user already have the role.
+        if (member.roles.find(x => x.id == ApprenticeKey)) return
+
+        //    Assign key, send notification.
+    	bot.logger.info(`${user.author.tag} has received Apprenticeship Key.`)
+        await member.addRole(ApprenticeKey)
+        return classRoomChannel.send(`${user.author} has joined the classroom!`)
+}
