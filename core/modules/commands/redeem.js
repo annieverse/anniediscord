19011184d18beee166950fb7a98dd6056ef7e211
Naph <nightcore.raven07@gmail.com>
2019-10-01 01:48:26 +0700

    redeem db fix

--- core/modules/commands/redeem.js
+++ core/modules/commands/redeem.js
@@ -13,7 +13,7 @@ class Redeem {
      *  Initializer method
      */
 	async execute() {
-		const { args, commanifier, bot:{db}, palette, emoji, trueInt, collector, reply, code: {REDEEM}, meta: {data, author} } = this.stacks
+		const { args, commanifier, db, palette, emoji, trueInt, collector, reply, code: {REDEEM}, meta: {data, author} } = this.stacks
 
 
 		//  Returns as short-guide if user doesn't specify any parameters
@@ -56,9 +56,9 @@ class Redeem {
 					//  Add collector fortune buffs if user has rany card
 					if (data.rany_card) {
 						//  Update Lucky Tickets
-						await this.db.addLuckyTickets(amount * 2)
+						await db(author.id).addLuckyTickets(amount * 2)
 						//  Withdraw artcoins
-						await this.db.withdraw(price, 52)
+						await db(author.id).withdraw(price, 52)
 
 						//  Redeem successful
 						return reply(REDEEM.COLLECTOR_FORTUNE, {
@@ -69,9 +69,9 @@ class Redeem {
 					}
 
 					//  Update Lucky Tickets
-					db.setUser(author.id).addLuckyTickets(amount)
+					db(author.id).addLuckyTickets(amount)
 					//  Withdraw artcoins
-					db.setUser(author.id).withdraw(price, 52)
+					db(author.id).withdraw(price, 52)
 
 
 					//  Redeem successful
