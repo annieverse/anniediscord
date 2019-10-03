d5ed527ab0f0abac3210e536a67feb12d985f576
Naph <nightcore.raven07@gmail.com>
2019-10-03 15:55:28 +0700

    enabled classroom

--- core/events/messageReactionAdd.js
+++ core/events/messageReactionAdd.js
@@ -1,5 +1,7 @@
 const Heart = require(`../utils/artFeaturingManager`)
+const Classroom = require(`../utils/classroomManager`)
 
 module.exports = async(Components) => {
     new Heart(Components).Add()
+    Classroom(Components)
 }
