641dd2f3b676f2f628637821ace0ec0d8344de47
Naph <nightcore.raven07@gmail.com>
2019-10-03 23:14:04 +0700

    disable classroom

--- core/events/messageReactionAdd.js
+++ core/events/messageReactionAdd.js
@@ -1,7 +1,5 @@
 const Heart = require(`../utils/artFeaturingManager`)
-const Classroom = require(`../utils/classroomManager`)
 
 module.exports = async(Components) => {
     new Heart(Components).Add()
-    Classroom(Components)
 }
