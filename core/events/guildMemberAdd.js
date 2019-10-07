ce8f7e624d1ec62cd4561422cdd2bf8e42876e90
BaitGod01 <Baitgod01@gmail.com>
2019-10-07 18:00:34 -0400

    added auto give of unverified and moved validating user to memberadd

--- core/events/guildMemberAdd.js
+++ core/events/guildMemberAdd.js
@@ -1,12 +1,19 @@
 const Banner = require(`../utils/welcomeBannerUI`)
 
-module.exports = (bot, member) => {
+module.exports = async (bot, member) => {
 	/*
             * @guildMemberAdd listener.
             * it will send canvas-generated message to welcome channel
             * for every joined user.
     */
-	new Banner({bot:bot, member:member, channel:`459891664182312982`}).render()
-	member.addRole(`460826503819558914`)
+    new Banner({bot:bot, member:member, channel:`459891664182312982`}).render()
+    
+    //	Register new data if its a new user, else ignore.
+    await bot.db.validatingNewUser(member.user.id)
+
+    // Add Pencilician role
+    member.addRole(`460826503819558914`)
+    // Add unverified role 
+    member.addRole(`588663266805415936`)
 
 }
