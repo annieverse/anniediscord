c58c662c24c76e904f53e774d10daf664ad20df4
hugonun <43740923+hugonun@users.noreply.github.com>
2019-10-14 01:25:00 +0200

    linted

--- core/utils/ScareTheMascot.js
+++ core/utils/ScareTheMascot.js
@@ -10,13 +10,13 @@ class ScareTheMascot {
 
         this.active = false // on/off switch
         this.eventchannels = [
-            '614737097454125056',
-            '628330270755651584'
+            `614737097454125056`,
+            `628330270755651584`
         ]
         this.currentevent = false
-        this.eventchannel = ''
-        this.collectortrigger = '👻'
-        this.prefix = '👻ˋˏ┋ '
+        this.eventchannel = ``
+        this.collectortrigger = `👻`
+        this.prefix = `👻ˋˏ┋ `
         this.scaryusers = []
     }
 
@@ -43,18 +43,17 @@ class ScareTheMascot {
             await this.cleanevent()
         }
         // Select random channel for event
-        this.eventchannel = this.eventchannels[Math.floor(Math.random() * this.eventchannels.length)];
+        this.eventchannel = this.eventchannels[Math.floor(Math.random() * this.eventchannels.length)]
         await this.addChannelPrefix(this.eventchannel)
         // TODO Needs prettier message
-        this.bot.channels.get(this.eventchannel).send('Boo!')
-        this.logger.info('Scare The Mascot - Starting event on channel ' + this.eventchannel)
+        this.bot.channels.get(this.eventchannel).send(`Boo!`)
+        this.logger.info(`Scare The Mascot - Starting event on channel ` + this.eventchannel)
         this.currentevent = true
 
         // Collector
-        const collector = new MessageCollector(this.bot.channels.get(this.eventchannel), m => m.content.includes(this.collectortrigger), { time: 60*1000 });
-        collector.on('collect', message => {
+        const collector = new MessageCollector(this.bot.channels.get(this.eventchannel), m => m.content.includes(this.collectortrigger), { time: 60*1000 })
+        collector.on(`collect`, message => {
             if(!message.author.bot){
-                console.log(message.content)
                 this.scaryusers.push(message.author.id)
             }
         })
@@ -64,19 +63,19 @@ class ScareTheMascot {
         if(this.currentevent) {
             // Get scary users
             let winnerlist = [...new Set(this.scaryusers)]
-            this.logger.info('Scare The Mascot - Users participating: ' + winnerlist)
+            this.logger.info(`Scare The Mascot - Users participating: ` + winnerlist)
 
 
             // Grab winner and reward
             if(this.scaryusers.length < 1) {
-                this.logger.info('Scare The Mascot - No one won')
-                this.bot.channels.get(this.eventchannel).send('No one won.')
+                this.logger.info(`Scare The Mascot - No one won`)
+                this.bot.channels.get(this.eventchannel).send(`No one won.`)
             } else {
-                const winner = winnerlist[Math.floor(Math.random() * winnerlist.length)];
+                const winner = winnerlist[Math.floor(Math.random() * winnerlist.length)]
                 const db = new databaseManager(winner)
                 db.storeCandies(10)
-                this.logger.info('Scare The Mascot - ' + winner + ' wins <amount> candies!')
-                this.bot.channels.get(this.eventchannel).send(winner + ' won!')
+                this.logger.info(`Scare The Mascot - ` + winner + ` wins <amount> candies!`)
+                this.bot.channels.get(this.eventchannel).send(winner + ` won!`)
             }
         }
         await this.cleanevent()
@@ -89,7 +88,7 @@ class ScareTheMascot {
         this.eventchannels.forEach((i) => {this.deleteChannelPrefix(i)})
         // Adjust vars
         this.currentevent = false
-        this.eventchannel = ''
+        this.eventchannel = ``
         this.scaryusers = []
     }
 
@@ -100,13 +99,12 @@ class ScareTheMascot {
 
     async deleteChannelPrefix(channelid) {
         let channel = this.bot.channels.get(channelid)
-        channel.setName(channel.name.replace(new RegExp(this.prefix + "(.*)", "g"),'$1'))
+        channel.setName(channel.name.replace(new RegExp(this.prefix + `(.*)`, `g`),`$1`))
     }
 
     async delay(ms) {
-        return new Promise(function (resolve) { return setTimeout(resolve, ms); });
-    };
-
+        return new Promise(function (resolve) { return setTimeout(resolve, ms) })
+    }
 
 }
 
