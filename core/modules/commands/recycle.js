664a1e87fc9810172c7870332c3983575405d095
BaitGod01 <Baitgod01@gmail.com>
2019-11-05 01:14:08 -0500

    recycle command finished?

--- core/modules/commands/recycle.js
+++ core/modules/commands/recycle.js
@@ -2,6 +2,14 @@ class mainHub {
 	constructor(Stacks) {
 		this.stacks = Stacks
 		this.messageIds = []
+		this.recycleEchangeMeta = {}
+		this.rarityLevels = {
+			1: 5,
+			2: 4,
+			3: 3,
+			4: 2,
+			5: 1
+		}
 	}
 
 	set messageIDs(item){
@@ -15,9 +23,16 @@ class mainHub {
 	async userInventory(){
 		const { bot: { db }, meta: { author } } = this.stacks
 		let inventory = await db.pullInventory(author.id)
+		let inventoryRaw = inventory
 		inventory = db._transformInventory(inventory)
+		let values = Object.values(inventory)
+		let keys = Object.keys(inventory)
+		values.forEach((element,index)=>{
+			if (element < 1) delete inventory[keys[index]]
+		})
 		inventory = Object.keys(inventory)
-		return inventory
+		
+		return {inventory,inventoryRaw}
 	}
 
 	async userInventoryCards(){
@@ -51,57 +66,123 @@ class mainHub {
 	async getRecycleItems(){
 		const { bot: { db } } = this.stacks
 		let recycleRaw = await db.recycleableItems
-		let recycleData = []
+		let recycleAlias = [], recycleData = []
 		recycleRaw.forEach(element => {
-			recycleData.push(element.alias)
+			recycleAlias.push(element.alias)
+			recycleData.push(element)
 		})
-		return recycleData
+		return {recycleAlias,recycleData}
+	}
+
+	async getRecyclesInInventory(){
+		let inventory = await this.userInventory()
+		inventory = inventory.inventory
+		let recycleableItems = await this.getRecycleItems()
+		let itemsInInventory = []
+		for (let index = 0; index < recycleableItems.recycleAlias.length; index++) {
+			for (let secondIndex = 0; secondIndex < inventory.length; secondIndex++) {
+				if (recycleableItems.recycleAlias[index] == inventory[secondIndex]) itemsInInventory.push(inventory[secondIndex])
+			}
+		}
+		return itemsInInventory
 	}
 
 	async userHasItemsToRecycle(){
 		let inventory = await this.userInventory()
+		inventory = inventory.inventory
 		let recycleableItems = await this.getRecycleItems()
 		let answers = []
-		for (let index = 0; index < recycleableItems.length; index++) {
-			answers.push(inventory.includes(recycleableItems[index]))
+		for (let index = 0; index < recycleableItems.recycleAlias.length; index++) {
+			answers.push(inventory.includes(recycleableItems.recycleAlias[index]))
 		}
 		return answers.includes(true) ? true : false
 	}
 
 	get cleanMsgTransaction(){
 		const { message } = this.stacks
-		return message.channel.bulkDelete(this.messageIds)
+		return message.channel.bulkDelete(this.messageIDs)
+	}
+
+	async buildRepMetadata(recyclesInInv){
+		
+		let recycleRarity = []
+		let recycleDataRaw = await this.getRecycleItems()
+		let recycleData = recycleDataRaw.recycleData
+		for (let index = 0; index < recycleData.length; index++) {
+			for (let secondIndex = 0; secondIndex < recyclesInInv.length; secondIndex++) {
+				if (recycleData[index].alias == recyclesInInv[secondIndex]) {
+					recycleRarity.push(recycleData[index].rarity)
+					this.recycleEchangeMeta[recycleData[index].alias] = this.rarityLevels[recycleData[index].rarity]
+				}
+			}
+		}
 	}
 
 	async execute() {
-		const { message, reply, code: { RECYCLE } } = this.stacks
+		const { message, trueInt,collector, emoji, reply, code: { RECYCLE } ,bot:{db} } = this.stacks
 
-		this.messageIds.push(message.id)
+		this.messageIDs = message.id
 
 		// user still has cards to craft
 		let hasCards = await this.userHasNoMoreCardsToCraft()
-		if (!hasCards) return reply(RECYCLE.STILL_HAS_CARDS_TO_CRAFT)
+		if (!hasCards) return reply(RECYCLE.STILL_HAS_CARDS_TO_CRAFT).then(msg => this.messageIDs = msg.id).then(this.cleanMsgTransaction)
 		
 		// user has no items to craft
 		let hasRecycles = await this.userHasItemsToRecycle()
-		if (!hasRecycles) return reply(RECYCLE.HAS_NO_ITEMS_TO_RECYCLE)
+		if (!hasRecycles) return reply(RECYCLE.HAS_NO_ITEMS_TO_RECYCLE).then(msg => this.messageIDs = msg.id).then(this.cleanMsgTransaction)
 		
-		new cleanInv(this.stacks).execute()
-	}
-}
+		let recyclesInInv = await this.getRecyclesInInventory()
+		await this.buildRepMetadata(recyclesInInv)
+		
+		let inventory = await this.userInventory()
+		inventory = inventory.inventoryRaw
+		var displayItems=``
+		for (let index = 0; index < recyclesInInv.length; index++) {
+			for (let secondIndex = 0; secondIndex < inventory.length; secondIndex++) {
+				// eslint-disable-next-line no-useless-escape
+				if (recyclesInInv[index] == inventory[secondIndex].alias && inventory[secondIndex].quantity > 0) displayItems += `> ${emoji(recyclesInInv[index])}**${recyclesInInv[index].replace(/\_/g, ` `)} x ${inventory[secondIndex].quantity}**\n`
+			}
+			
+		}
+		displayItems += `\n\n Above are all your available recyceables. Please type ** <amount> <itemname>** to recycle.`
+		let metadata = {}
+		reply(displayItems).then(msg => this.messageIDs = msg.id)
+		collector.on(`collect`, async (msg) => {
+			this.messageIDs = msg.id
+			const input = msg.content.toLowerCase()
+			const params = input.split(` `)
 
-class cleanInv extends mainHub {
-	constructor(Stacks) {
-		super(Stacks)
-		this.stacks = Stacks
-	}
 
-	async execute(){
-		const { reply } = this.stacks
+			//  Get amount of gift to send from first parameter
+			metadata.amount_to_send = trueInt(params[0])
+			//  Get type of item to send from second parameter. Ignoring spaces.
+			// eslint-disable-next-line no-useless-escape
+			metadata.item_to_send = input.slice(input.indexOf(params[1])).replace(/\ /g, `_`)
+			//  Returns if format is invalid
+			if (!metadata.amount_to_send && !metadata.item_to_send) return reply(RECYCLE.INVALID_FORMAT)
 
-		reply(`hmm`).then(msg => super.messageIds.push(msg.id))
+			if (this.recycleEchangeMeta[metadata.item_to_send] == (null || undefined)) return reply(RECYCLE.INVALID_FORMAT)
 
-		//super.cleanMsgTransaction
+			
+			//  Total gained reps from given properties above
+			metadata.counted_reps = Math.floor(metadata.amount_to_send / this.recycleEchangeMeta[metadata.item_to_send])
+
+			if (metadata.counted_reps == 0) return reply(RECYCLE.NOT_ENOUGH_ITEMS, { socket: [metadata.item_to_send, this.recycleEchangeMeta[metadata.item_to_send] - metadata.amount_to_send] })
+			//  Closing the connections
+			collector.stop()
+
+			let itemData = await db.getItemMetadata(metadata.item_to_send)
+			//  Send reputation points
+			db.setUser(msg.author.id).storeArtcoins(metadata.counted_reps)
+			//  Withdraw sender's gifts
+			db.setUser(msg.author.id).withdraw(metadata.amount_to_send, itemData[0].itemId)
+
+			this.cleanMsgTransaction
+			//  recycling successful
+			// eslint-disable-next-line no-useless-escape
+			reply(`You have converted ${metadata.amount_to_send}, ${metadata.item_to_send.replace(/\_/g, ` `)}, into ${metadata.counted_reps} ${emoji(`artcoins`)}`)
+
+		})
 	}
 }
 
@@ -109,7 +190,7 @@ module.exports.help = {
 	start: mainHub, 
 	name:`recycle`,
 	aliases: [], 
-	description: `Have items you can't use, convert them to AC. *ratio is 5:1`,
+	description: `Have items you can't use, convert them to AC.`,
 	usage: `TemplateCommand`,
 	group: `shop`,
 	public: false,
