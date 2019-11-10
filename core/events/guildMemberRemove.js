ce9916c34321ec70abcbd48d273f1b4ace91ca50
BaitGod01 <Baitgod01@gmail.com>
2019-11-10 02:06:32 -0500

    got rid of member counter

--- core/events/guildMemberRemove.js
+++ core/events/guildMemberRemove.js
@@ -1,15 +1,6 @@
-const formatManager = require(`../utils/formatManager`)
 const { absence_ticket } = require(`../utils/role-list`)
 
 module.exports = (bot,member) => {
-	// "I've tweaked the interface for server stats. -naphnaphz / 12.01.18"
-	let members = member.guild.memberCount
-	let botSize = member.guild.members.filter(a => a.user.bot).size
-	let userSize = members - botSize
-	const configFormat = new formatManager()
-
-	let memberCountUpdate = bot.channels.get(`518245560239652867`)
-	memberCountUpdate.setName(`˗ˏˋ ${configFormat.threeDigitsComa(userSize)} Artists!! ˎˊ˗`)
 
 	userLeaving()
 
