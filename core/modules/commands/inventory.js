ac79cbf4bc578a69b2cba13a25c690d7a3b02a9b
BaitGod01 <Baitgod01@gmail.com>
2019-11-20 23:00:21 -0500

    text option for inventory

--- core/modules/commands/inventory.js
+++ core/modules/commands/inventory.js
@@ -16,7 +16,7 @@ class Inventory {
      *  Initialzer method
      */
 	async execute() {
-		const { code: {INVENTORY}, bot:{db}, name, reply, emoji } = this.stacks
+		const { command, code: {INVENTORY}, bot:{db}, name, reply, emoji } = this.stacks
 
 
 		//  Returns if user is invalid
@@ -27,26 +27,48 @@ class Inventory {
 			strict: true
 		})
 
-
+		function textOpt(inv){
+			let response = `Item: Quantity\n`
+			inv.forEach((element,index) => {
+				index == inv.length-1 ? response += `${element.name}: ${element.quantity}` : response += `${element.name}: ${element.quantity}\n`
+			});
+			return response
+		}
+		
 		//  Display result
-		return reply(INVENTORY.FETCHING, {socket: [name(this.author.id)], simplified: true})
-			.then(async load => {
+		command.includes(`text`) || command.includes(`tex`) || command.includes(`t`) || command.includes(`tx`) ? 
+			reply(INVENTORY.FETCHING, { socket: [name(this.author.id)], simplified: true })
+				.then(async load => {
 
-				reply(INVENTORY.HEADER, {
-					socket: [emoji(`AnnieYandere`), name(this.author.id)],
-					image: await GUI(Inventory, this.theme),
-					prebuffer: true,
-					simplified: true
+					reply(`${INVENTORY.HEADER}\n${textOpt(Inventory)}`, {
+						socket: [emoji(`AnnieYandere`), name(this.author.id)],
+						simplified: true
+					})
+					load.delete()
 				})
-				load.delete()
-			})
+			:
+			reply(INVENTORY.FETCHING, { socket: [name(this.author.id)], simplified: true })
+				.then(async load => {
+
+					reply(INVENTORY.HEADER, {
+						socket: [emoji(`AnnieYandere`), name(this.author.id)],
+						image: await GUI(Inventory, this.theme),
+						prebuffer: true,
+						simplified: true
+					})
+					load.delete()
+				})
 	}
 }
 
 module.exports.help = {
 	start: Inventory,
 	name: `inventory`,
-	aliases: [`inventory`, `inv`, `bag`, `invent`, `inven`],
+	aliases: [`inventory`, `inv`, `bag`, `invent`, `inven`,
+		`inventorytx`, `invtx`, `bagtx`, `inventtx`, `inventx`,
+		`inventorytex`, `invtex`, `bagtex`, `inventtex`, `inventex`,
+		`inventorytext`, `invtext`, `bagtext`, `inventtext`, `inventext`,
+		`inventoryt`, `invt`, `bagt`, `inventt`],
 	description: `Views your inventory`,
 	usage: `inventory`,
 	group: `General`,
