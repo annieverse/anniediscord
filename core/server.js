77d8bdc2f90a8fe7d94a9dd2ca95b0c6146c32e8
Naph <nightcore.raven07@gmail.com>
2019-12-01 00:54:39 +0700

    added incompatible dependencies check for production

--- core/server.js
+++ core/server.js
@@ -1,9 +1,6 @@
 module.exports = () => {
-	
-	//Initialize startup modules
-	const { Client } = require(`discord.js`)
-	let bot = new Client()
-	bot.startupInit = process.hrtime()
+
+	const environment = require(`../.data/environment`)
 	const ascii = require(`./utils/config/startupAscii`)
 	const benchmark = require(`./utils/benchmarkConverter`)
 	const modulesLoader = require(`./utils/modulesLoader`)
@@ -14,6 +11,25 @@ module.exports = () => {
 	const winston = require(`./utils/config/winston`)
 	const cards = require(`./utils/cards-metadata`)
 	const msgCodes = require(`./utils/predefinedMessages`)
+	const cmd = require(`node-cmd`)
+	const { dependencies } = require(`../package`)
+
+
+	//	Handling incompatible version of canvas.
+	if (!environment.dev) {
+		if (!dependencies.canvas.includes(`2.0.0-alpha.2`)) {
+			winston.info(`Incompatible version of canvas has been detected in the production, fallback version will be installed in a moment.`)
+			return cmd.run(`
+				npm install canvas@2.0.0-alpha.2
+			`)
+		}
+	}
+
+	
+	//Initialize startup modules
+	const { Client } = require(`discord.js`)
+	let bot = new Client()
+	bot.startupInit = process.hrtime()
 	const app = express()
 
 	
