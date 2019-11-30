1d92c26f34fd33982c4f7f921fad09b9b44fbc57
Naph <nightcore.raven07@gmail.com>
2019-11-30 22:45:14 +0700

    added interactive CLI in dev eval

--- core/modules/commands/eval.js
+++ core/modules/commands/eval.js
@@ -4,20 +4,57 @@
  * @DeveloperTool as function to runs custom code on the fly
  */
 const Discord = require(`discord.js`)
+const cmd = require(`node-cmd`)
 class DeveloperTool {
 	constructor(Stacks) {
 		this.stacks = Stacks
 	}
+
+	
+	/**
+     * CLI tool. Took boilerplate from dbkits.js
+     * @commandLineInterface
+     */
+	async commandLineInterface() {
+		const { fullArgs, reply, palette, code:{ DBKITS }, name, meta: {author} } = this.stacks
+
+		//	Return if user doesn't specify arguments.
+		if (!fullArgs) return reply(`please include the cmd`)
+
+		//	Parse statement
+		const stmt = fullArgs.match(/\[(.*?)\]/)[1]
+		//	Make sure the the stmt is valid
+		if (!stmt) return reply(DBKITS.MISSING_STMT)
+
+		
+		//	Parse flag
+		const flag = fullArgs.match(/[^--]*$/)[0].substring(0, 3)
+		//	Flag check as well
+		if (!flag) return reply(DBKITS.MISSING_FLAG)
+
+
+		//	Running query
+		return cmd[flag](stmt, function(err, data, stderr) {
+			if (err) return reply(`Something went wrong ...\n\n${err}`)
+			const parsedResult = (JSON.stringify(data)).substring(0, 1800).replace(/\,/g, `,\n`)
+		//	Display result
+		return reply(`\`\`\`json\n${parsedResult}\n\`\`\``)
+		})
+	}
+
 
 	/**
      * Initializer method
      * @Execute
      */
 	async execute() {
-		const {bot,palette, isDev, message, args, utils:{pages}, code: {EVAL}, reply} = this.stacks
+		const {bot:{db},palette, isDev, command, message, args, utils:{pages}, code: {EVAL}, reply} = this.stacks
 
 		//  Returns if the author is not in dev team or admin.
 		if (!isDev) return reply(EVAL.UNKNOWN_AUTHOR, { color: palette.red })
+
+		//	Run CLI when "cmd" prefix is used.
+		if (command.startsWith(`cmd`)) return this.commandLineInterface()
 
 		try {
 			let evaled = await eval(args.join(` `))
@@ -33,7 +70,7 @@ class DeveloperTool {
 module.exports.help = {
 	start: DeveloperTool,
 	name: `eval`,
-	aliases: [],
+	aliases: [`cmd`],
 	description: `evalutes a line of code`,
 	usage: `eval <what you want to test>`,
 	group: `Admin`,
