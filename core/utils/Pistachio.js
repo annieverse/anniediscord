769f61d7862223b84cbe1c751ce46b63e9bfdcd3
BaitGod01 <Baitgod01@gmail.com>
2020-01-20 18:59:34 -0500

    deploy fix?

--- core/utils/Pistachio.js
+++ core/utils/Pistachio.js
@@ -85,7 +85,7 @@ class Pistachio {
 
 			// Check for staff team authority
 			container.isStaff = message.member.roles.find(r => Object.keys(container.roles.teams).some(i => container.roles.teams[i] == r.id))
-
+ 
 			//  Check for booster user
 			container.isVIP = message.member.roles.find(r => r.id === `585550404197285889`)
 
