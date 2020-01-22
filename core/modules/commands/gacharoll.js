f80cbf0d2be4d524a18ffcc6a4c995d971a298f6
BaitGod01 <Baitgod01@gmail.com>
2020-01-22 17:03:23 -0500

    got rid of gacha channel restriction

--- core/modules/commands/gacharoll.js
+++ core/modules/commands/gacharoll.js
@@ -75,10 +75,7 @@ class Gacha {
 	 * 	Initializer
 	 */
 	async execute() {
-		const { message, name, reply, code:{GACHA}, choice, emoji, gachaField, isGachaField } = this.stacks
-
-		//	Returns if current channel is not in gacha-allowed list
-		if (!isGachaField) return reply(GACHA.UNALLOWED_ACCESS, {socket: [gachaField]})
+		const { message, name, reply, code:{GACHA}, choice, emoji} = this.stacks
 
 		//	Returns if user doesn't have any lucky ticket
 		if (!this.data.lucky_ticket) return reply(GACHA.ZERO_TICKET)
