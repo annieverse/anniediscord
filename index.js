6be440df28582484f6ea11307863287bd28bdcdf
Naph <nightcore.raven07@gmail.com>
2020-03-06 01:47:08 +0700

    progressing with klasa

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 require(`dotenv`).config()
-const Annie = require(`./src/annie`)
-
-/**
- * Feel free to change the method to .minimal() if you want to test out
- * lighter version of the app.
- * Keep in mind that majority of the features are disabled in minimalist-state
- */
-new Annie().default()
+require(`./src/annie`)
--- src/annie.js
+++ src/annie.js
@@ -8,94 +8,20 @@ const Keyv = require(`keyv`)
 const Express = require(`express`)
 const locale = require(`./locales/default`)
 const getBenchmark = require(`./utils/getBenchmark`)
-
-class Annie extends Discord.Client {
-	constructor() {
-		super({ disableEveryone: true })
-		this.startupInit = process.hrtime()
-		this.benchmark = getBenchmark
-		this.logger = logger
-		this.locale = locale
-		this.config = config
-		this.keyv = new Keyv()
-	}
-
-	
-	/**
-	 * @desc default option for deploying Annie's Client with its full features
-	 * such as database access and reachable interactions through custom commands.
-	 */
-	async default() {
-		try {
-			process.on(`unhandledRejection`, err => {
-				logger.error(`Promise Rejection > ${err.stack}`)
-			})
-
-			logger.debug(ascii.default)
-			logger.info(`Initializing ${process.env.NODE_ENV} server...`)
-
-			// Connecting to .sqlite file in .data/database.sqlite
-			await this._setupDatabase()
-			// Registering all the available commands from ./src/commands directory
-			await this._setupCommands()
-			// Listening to default port
-			this._listeningToPort(this.config.port)
-			// Listening to discordjs events
-			require(`./events/eventHandler`)(this)
-
-			// Logging in and trigger events/ready.js
-			logger.info(`Logging in...`)
-			this.login(process.env.TOKEN)
-		}
-		catch(error) {
-			logger.error(`Client terminated > ${error.stack}`)
-		}
-	}
-
-	
-	/**
-	 * @desc This method has eliminated some of processes such as database connection
-	 * and commands loader to reduce the client load.
-	 * Intentionally made for fun and internal monitoring without reachable interactions as end user.
-	 */
-	minimal() {
-		try {
-			logger.info(ascii.minimalist)
-			logger.info(`Initializing ${process.env.NODE_ENV} server...`)
-			logger.info(`Logging in...`)
-			this.login(process.env.TOKEN)
-		}
-		catch(error) {
-			return logger.error(`Client terminated > ${error.stack}`)
-		}
-	}
+const { Client } = require(`klasa`)
 
 
-	async _setupDatabase() {
-		const dbtime = process.hrtime()
-		const db = await new Database().connect()
-		await db.validatingTables()
-		// Assign once successful
-		this.db = db
-		logger.info(`Database connected (${getBenchmark(dbtime)})`)
-	}
+logger.debug(ascii.default)
+logger.info(`Initializing ${process.env.NODE_ENV} server...`)
 
+process.on(`unhandledRejection`, err => {
+	logger.error(`Promise Rejection > ${err.stack}`)
+})
 
-	async _setupCommands() {
-		const cmdtime = process.hrtime()
-		this.commands = await new CommandsLoader().default()
-		logger.info(`${this.commands.totalFiles} commands registered (${getBenchmark(cmdtime)})`)
-	}
-
-
-	_listeningToPort(port=0) {
-		const app = Express()
-		app.get(`/`, (request, response) => response.sendStatus(200))
-		app.listen(port)
-		logger.info(`Listening to port ${port}`)
-	}
-
-
-}
-
-module.exports = Annie
+new Client({
+    fetchAllMembers: false,
+    prefix: config.prefix,
+    commandEditing: true,
+    typing: true,
+    readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
+}).login(process.env.TOKEN)
