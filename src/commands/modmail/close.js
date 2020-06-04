6e63d4c0486d7cfd88f9f39fe2fd145e3e206671
pan <baitgod01@gmail.com>
2020-06-04 11:10:33 -0400

    modmail pluged in except in message controller

---
+++ src/commands/modmail/close.js
@@ -0,0 +1,108 @@
+const Command = require(`../../libs/commands`)
+const modmailConfig = require(`./modmailConfig.json`)
+/**
+ * Command's Class description
+ * @author The Frying Pan
+ */
+class closeThread extends Command {
+
+    /**
+     * @param {external:CommandComponents} Stacks refer to Commands Controller.
+     */
+    constructor(Stacks) {
+        super(Stacks)
+        this.stacks = Stacks
+        this.modmailCategory = modmailConfig.category
+    }
+
+    /**
+     * Running command workflow
+     * @param {PistachioMethods} Object pull any pistachio's methods in here.
+     */
+    async execute({reply}) {
+        
+        const isDm = this.message.channel.type == `dm`
+        const requestMadeInModmailCategory = this.message.channel.parentID == this.modmailCategory
+
+        if (isDm){ 
+            await this.alreadyOpenThread(reply, true)        
+        } else if (requestMadeInModmailCategory){
+            await this.alreadyOpenThread(reply, false)
+        }
+    }
+
+    /**
+     * Looks for a open thread, then triggers the event to close thread if one is found
+     * @param {reply} reply 
+     * @param {boolean} dm 
+     */
+    async alreadyOpenThread(reply, dm){
+        
+        let search 
+        // setup ifs 
+        const emptySearchResults = search == `none`
+        const messageFromDm = this.message.channel.type == `dm`
+        const requestMadeInModmailCategory = this.message.channel.parentID == this.modmailCategory
+
+        // if request from dm, use message.author.id, if from modmail category use channel.id
+        if (messageFromDm){ 
+           search = await this.bot.db.alreadyOpenThread(this.message.author.id, dm)
+        } else if (requestMadeInModmailCategory){
+            search = await this.bot.db.alreadyOpenThread(this.message.channel.id, dm)
+        }
+        
+        if (emptySearchResults){
+            if (messageFromDm){
+                return reply(`There is currently no open thread, type a new message to start one :)`)
+            }
+        } else {
+            // Close thread
+            this.closeThread(search)
+            if (messageFromDm){
+                reply(`This thread has been closed.`)
+            } else {
+                this.bot.guilds.get(search.guild_id).members.get(search.user_id).send(`This thread has been closed.`)
+            }
+           
+        }
+    }
+
+    /**
+     * Update the database and delete the channel
+     * @param {object} thread 
+     * @returns nothing
+     */
+    async closeThread(thread){
+        this.bot.db.closeThread(thread.thread_id)
+        this.bot.guilds.get(thread.guild_id).channels.get(thread.channel).delete()
+        this.logEvent(thread)
+    }
+
+    /**
+     * Send a message to the log channel indicating the thread closed and give the thread id
+     */
+    logEvent(threadTicket){
+        let threadUser = this.bot.guilds.get(threadTicket.guild_id).members.get(threadTicket.user_id) 
+        let member = {
+            username: threadTicket.is_anonymous == 0 ? `${threadUser.user.username}#${threadUser.user.discriminator}` : `anonymous`,
+            accountAge: threadUser.user.createdAt, 
+            id: threadTicket.is_anonymous == 0 ? threadUser.id : `anonymous`,
+            nickname: threadTicket.is_anonymous == 0 ? threadUser.nickname : `anonymous`,
+            joined: threadUser.joinedAt
+        }
+        this.bot.guilds.get(threadTicket.guild_id).channels.get(modmailConfig.logChannel).send(`Modmail thread with ${member.username} (${member.id}) was closed by ${this.message.author.username}\nLog ID: ${threadTicket.thread_id}`)
+    }
+}
+
+
+module.exports.help = {
+    start: closeThread,
+    name: `close`,
+    aliases: [`closethread`],
+    description: `test`,
+    usage: `close`,
+    group: `modmail`,
+    permissionLevel: 0,
+    public: true,
+    multiUser: false
+}
--- src/libs/database.js
+++ src/libs/database.js
@@ -450,6 +450,222 @@ class Database {
 		return res.timestamp
 	}
 
+	/**
+	 * MODMAIL Plugin
+	 */
+    writeToThread(user_id, mod_id, guild_id, thread_id, text){
+        this._query(`
+			INSERT INTO modmail_thread_messages (registered_at, user_id, mod_id, guild_id, thread_id, message)
+			VALUES (datetime('now'), ?, ?, ?, ?, ?)`
+			, `run`
+			, [user_id, mod_id, guild_id, thread_id, text]
+			)
+	}
+
+	getBlockedUsers(){
+		return this._query(`
+				SELECT user_id
+				FROM modmail_blocked_users
+				WHERE blocked = 1`
+				, `all`
+				, []
+				,`getting blocked users`
+			)
+	}
+
+	getBlockedUserReason(user_id){
+		return this._query(`
+				SELECT reason
+				FROM modmail_blocked_users
+				WHERE user_id = ?`
+				, `get`
+				, [user_id]
+				,`getting blocked user's reason`
+			)
+	}
+
+	getBlockedUsersList(){
+		return this._query(`
+			SELECT user_id
+			FROM modmail_blocked_users`
+			, `all`
+			, []
+			,`getting blocked users`
+		)
+	}
+
+	registerUserInBlockList(user_id){
+		this._query(`
+			INSERT INTO modmail_blocked_users (registered_at, user_id, blocked)
+			VALUES (datetime('now'), ?, 0)`
+			, `run`
+			, [user_id]
+			)
+	}
+
+	blockUser(user_id, reason = `The Moderator didn't supply a reason, if you would like to appeal this block please address it to the mods on the server or owner.`){
+		this._query(`
+			UPDATE modmail_blocked_users 
+			SET blocked = 1 AND reason = ?
+			WHERE user_id = ?`
+			, `run`
+			, [user_id, reason]
+			, `blocking user`
+			)
+	}
+
+	unblockUser(user_id){
+		this._query(`
+			UPDATE modmail_blocked_users 
+			SET blocked = 0
+			WHERE user_id = ?`
+			, `run`
+			, [user_id]
+			)
+	}
+
+	isblockedUser(id){
+		this._query(`
+				SELECT *
+				FROM modmail_blocked_users
+				WHERE user_id = ?
+				AND blocked = 0`
+				, `get`
+				, [id]
+				,`checking if user is blocked`
+			)
+	}
+
+	async alreadyOpenThread(id, dm = true){
+		let search
+		if (dm) {
+			search = await this._query(`
+				SELECT *
+				FROM modmail_threads
+				WHERE user_id = ?
+				AND status = 'open'
+				ORDER BY thread_id`
+				, `get`
+				, [id]
+				,`getting open thread`
+			)
+		} else if (!dm) {
+			search = await this._query(`
+				SELECT *
+				FROM modmail_threads
+				WHERE channel = ?
+				AND status = 'open'
+				ORDER BY thread_id`
+				, `get`
+				, [id]
+				,`getting open thread`
+			)
+		}
+		if (!search) search = `none`
+		return search
+	}
+
+	updateChannel(id, threadId){
+		this._query(`
+				UPDATE modmail_threads
+				SET channel = ?
+				WHERE thread_id = ?`
+				, `run`
+				, [id, threadId]
+			)
+	}
+
+	getlogsForUser(userId){
+		return this._query(`
+			SELECT *
+			FROM modmail_threads
+			WHERE user_id = ?
+			AND status = 'closed' AND is_anonymous = 0
+			ORDER BY thread_id`
+			, `all`
+			, [userId]
+			,`getting logs for user`
+		)
+	}
+
+	closeThread(thread_id){
+		this._query(`
+		UPDATE modmail_threads SET status = 'closed'
+		WHERE thread_id = ?`
+		, `run`
+		, [thread_id]
+		)
+	}
+
+	makeNewThread(user_id, guild_id, thread_id, status, is_anonymous){
+		try {
+			this._query(`
+			INSERT INTO modmail_threads (registered_at, user_id, guild_id, thread_id, status, is_anonymous)
+			VALUES (datetime('now'), ?, ?, ?, ?, ?)`
+			, `run`
+			, [user_id, guild_id, thread_id, status, is_anonymous]
+			)
+		} catch (error) {
+			thread_id = makeRandomId()
+			this.makeNewThread(user_id, guild_id, thread_id, status, is_anonymous)
+		}
+		
+		function makeRandomId(){
+			var result = ``
+			var characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`
+			var charactersLength = characters.length
+			for ( var i = 0; i < 20; i++ ) {
+				result += characters.charAt(Math.floor(Math.random() * charactersLength))
+			}
+			return result
+		}
+	}
+
+	deleteLog(id) {
+		this._query(`
+			DELETE FROM modmail_thread_messages
+			WHERE thread_id = ?`
+			, `run`
+			, [id]
+			,`delete logs for specific thread`
+		)
+		this._query(`
+			DELETE FROM modmail_threads
+			WHERE thread_id = ?`
+			, `run`
+			, [id]
+			,`delete logs for specific thread`
+		)
+	}
+
+	getLogByThreadId(id){
+		return this._query(`
+			SELECT *
+			FROM modmail_thread_messages
+			WHERE thread_id = ?`
+			, `all`
+			, [id]
+			,`getting logs for specific thread`
+		)
+	}
+	
+	getThreadTicket(id){
+		return this._query(`
+			SELECT *
+			FROM modmail_threads
+			WHERE thread_id = ?`
+			, `get`
+			, [id]
+			,`getting thread data for specific thread`
+		)
+	}
+
+	/**
+	 * 
+	 * END OF MODMAIL PLUGIN
+	 * 
+	 */
+
 	/** -------------------------------------------------------------------------------
 	 *  User's Experience Points Method
 	 *  -------------------------------------------------------------------------------
@@ -1219,6 +1435,51 @@ class Database {
 	 * @returns {boolean}
 	 */
 	async verifyingTables() {
+		
+		/**
+		 * --------------------------
+		 * Modmail Plugin
+		 * --------------------------
+		 */
+		await this._query(`CREATE TABLE IF NOT EXISTS modmail_threads (
+			'registered_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			'user_id' TEXT NOT NULL,
+			'guild_id' TEXT NOT NULL,
+			'thread_id' REAL UNIQUE NOT NULL,
+			'status' TEXT NOT NULL,
+			'is_anonymous' INTEGER NOT NULL DEFAULT 0,
+			'channel' TEXT NOT NULL UNIQUE DEFAULT 0)`
+            , `run`
+			, []
+			, `Verifying table modmail_threads`)
+			
+		await this._query(`CREATE TABLE IF NOT EXISTS modmail_thread_messages (
+			'registered_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			'user_id' TEXT NOT NULL,
+			'mod_id' TEXT NOT NULL,
+			'guild_id' TEXT NOT NULL,
+			'thread_id' TEXT NOT NULL,
+			'message' TEXT NOT NULL)`
+            , `run`
+			, []
+			, `Verifying table modmail_thread_messages`)
+
+			
+		await this._query(`CREATE TABLE IF NOT EXISTS modmail_blocked_users (
+			'registered_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			'user_id' TEXT NOT NULL UNIQUE,
+			'blocked' INTEGER DEFAULT 0,
+			'reason' TEXT DEFAULT 'The Moderator didnt supply a reason, if you would like to appeal this block please address it to the mods on the server or owner.')`
+            , `run`
+			, []
+			, `Verifying table modmail_blocked_users`)
+			
+		/**
+		 * 
+		 * END OF MODMAIL PLUGIN
+		 * 
+		 */
+
 		/**
 		 * --------------------------
 		 * USER TREE
