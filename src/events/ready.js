39b11d7bb187f7f06bb8e572c31cae41549119e6
Naph <nightcore.raven07@gmail.com>
2020-08-17 02:55:36 +0700

    fix(ready): wrong caching on presencestatus

--- src/events/ready.js
+++ src/events/ready.js
@@ -23,9 +23,9 @@ module.exports = annie => {
 		 * 	--------------------------------------------------
 		 */
 		logger.info(`Successfully logged in. (${annie.getBenchmark(process.hrtime(annie.startupInit))})`)
-		logger.info(`currently serving in ${annie.guilds.size} guilds and ${annie.users.size} users`)
+		logger.info(`currently serving in ${annie.guilds.cache.size} guilds and ${annie.users.size} users`)
 		annie.user.setStatus(`online`)
-		annie.user.setActivity(`${commanifier(annie.users.size)} users | ${prefix}help`, {type: `WATCHING`})
+		annie.user.setActivity(`${commanifier(annie.users.cache.size)} users | ${prefix}help`, {type: `WATCHING`})
 		/**
 		 * 	--------------------------------------------------
 		 * 	Primary task
