1d60074673be7206a929bc9b9eee7cfee4e2cfc2
Naph <nightcore.raven07@gmail.com>
2020-09-25 19:36:20 +0700

    fix(raw): wrong refs

--- src/events/raw.js
+++ src/events/raw.js
@@ -6,15 +6,15 @@ module.exports = (bot, packet) => {
 	// Grab the channel to check the message from
 	const channel = bot.channels.cache.get(packet.d.channel_id)
 	// There's no need to emit if the message is cached, because the event will fire anyway for that
-	if (channel.messages.has(packet.d.message_id)) return
+	if (channel.messages.cache.has(packet.d.message_id)) return
 	// Since we have confirmed the message is not cached, let's fetch it
-	channel.messages.fetch(packet.d.message_id).then(message => {
+	channel.messages.cache.fetch(packet.d.message_id).then(message => {
 		// Emojis can have identifiers of name:id format, so we have to account for that case as well
 		const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name
 		// This gives us the reaction we need to emit the event properly, in top of the message object
-		const reaction = message.reactions.get(emoji)
+		const reaction = message.reactions.cache.get(emoji)
 		// Adds the currently reacting user to the reaction's users collection.
-		if (reaction) reaction.users.set(packet.d.user_id, bot.users.cache.get(packet.d.user_id))
+		if (reaction) reaction.users.cache.set(packet.d.user_id, bot.users.cache.get(packet.d.user_id))
 		// Check which type of event it is before emitting
 
 		if (packet.t === `MESSAGE_REACTION_ADD`) {
