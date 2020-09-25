4a30a577b6f40910b70f60e031019f493241a698
Naph <nightcore.raven07@gmail.com>
2020-09-25 19:43:27 +0700

    fix(raw): cant pull fetch from cached message

--- src/events/raw.js
+++ src/events/raw.js
@@ -8,7 +8,7 @@ module.exports = (bot, packet) => {
 	// There's no need to emit if the message is cached, because the event will fire anyway for that
 	if (channel.messages.cache.has(packet.d.message_id)) return
 	// Since we have confirmed the message is not cached, let's fetch it
-	channel.messages.cache.fetch(packet.d.message_id).then(message => {
+	channel.messages.fetch(packet.d.message_id).then(message => {
 		// Emojis can have identifiers of name:id format, so we have to account for that case as well
 		const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name
 		// This gives us the reaction we need to emit the event properly, in top of the message object
