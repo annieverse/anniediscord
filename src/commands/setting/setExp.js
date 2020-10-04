cbdc6fcf0369e02b7d7bc1859f5f26df4cb347d2
Naph <nightcore.raven07@gmail.com>
2020-10-04 15:47:07 +0700

    feat(setExp): command that allows you to enable or disable the exp leveling system

--- src/commands/setting/setExp.js
+++ src/commands/setting/setExp.js
@@ -1,10 +1,10 @@
 const Command = require(`../../libs/commands`)
 const moment = require(`moment`)
 /**
- * Enable or disable level-up message module for this guild
+ * Enable or disable EXP Leveling System for this guild
  * @author klerikdust
  */
-class SetLevelupMessage extends Command {
+class SetExp extends Command {
 
     /**
      * @param {external:CommandComponents} Stacks refer to Commands Controller.
@@ -12,7 +12,7 @@ class SetLevelupMessage extends Command {
     constructor(Stacks) {
         super(Stacks)
         /**
-         * An array of the available options for welcomer module
+         * An array of the available options for EXP_MODULE module
          * @type {array}
          */
         this.actions = [`enable`, `disable`]
@@ -27,7 +27,7 @@ class SetLevelupMessage extends Command {
          * Current instance's config code
          * @type {string}
          */  
-        this.primaryConfigID = `LEVEL_UP_MESSAGE`
+        this.primaryConfigID = `EXP_MODULE`
     }
 
     /**
@@ -37,18 +37,18 @@ class SetLevelupMessage extends Command {
     async execute({ reply, name, emoji }) {
         await this.requestUserMetadata(1)
         //  Handle if user doesn't specify any arg
-        if (!this.fullArgs) return reply(this.locale.SETLEVELUPMESSAGE.GUIDE, {
+        if (!this.fullArgs) return reply(this.locale.SETEXP.GUIDE, {
             color: `crimson`,
             thumbnail: this.thumbnail,
             header: `Hi, ${name(this.user.id)}!`,
             socket: {
                 prefix: this.bot.prefix,
-                emoji: emoji(`AnnieGeek`)
+                emoji: emoji(`AnnieDab`)
             }
         })
         //  Handle if the selected options doesn't exists
         this.selectedAction = this.args[0].toLowerCase()
-        if (!this.actions.includes(this.selectedAction)) return reply(this.locale.SETLEVELUPMESSAGE.INVALID_ACTION, {
+        if (!this.actions.includes(this.selectedAction)) return reply(this.locale.SETEXP.INVALID_ACTION, {
             socket: {actions: this.actions.join(`, `)},
             status: `fail`
         })   
@@ -59,17 +59,22 @@ class SetLevelupMessage extends Command {
     }
 
     /**
-     * Enabling levelup-message module
+     * Enabling EXP Leveling Module
      * @param {PistachioMethods} Object pull any pistachio's methods in here.
      * @returns {Pistachio.reply}
      */
-    async enable({ reply, name }) {
-        const fn = `[setLevelupMessage.enable()]`
+    async enable({ reply, name, emoji }) {
+        const fn = `[setExp.enable()]`
         //  Handle if module already enabled before the action.
         if (this.primaryConfig.value) {
+            //  Handle if module used the default value.
+            if (!this.primaryConfig.setByUserId) return reply(this.locale.SETEXP.ALREADY_ENABLED_BY_DEFAULT, {
+                socket: {emoji: emoji(`AnniePout`)},
+                color: `crimson`
+            })
             const now = moment()
             const localizeTime = await this.bot.db.toLocaltime(this.primaryConfig.updatedAt)
-            return reply(this.locale.SETLEVELUPMESSAGE.ALREADY_ENABLED, {
+            return reply(this.locale.SETEXP.ALREADY_ENABLED, {
                 status: `warn`,
                 socket: {
                     user: name(this.primaryConfig.setByUserId),
@@ -86,21 +91,21 @@ class SetLevelupMessage extends Command {
             cacheTo: this.guildConfigurations
         })
         this.logger.info(`${fn} ${this.primaryConfigID} for GUILD_ID:${this.message.guild.id} has been enabled.`)
-        return reply(this.locale.SETLEVELUPMESSAGE.SUCCESSFULLY_ENABLED, {
+        return reply(this.locale.SETEXP.SUCCESSFULLY_ENABLED, {
             socket: {prefix: this.bot.prefix},
             status: `success`
         })
     }
 
     /**
-     * Disabling levelup-message module
+     * Disabling EXP Leveling Module
      * @param {PistachioMethods} Object pull any pistachio's methods in here.
      * @returns {Pistachio.reply}
      */
-    async disable({ reply, emoji }) {
-        const fn = `[setLevelupMessage.disable()]`
+    async disable({ reply }) {
+        const fn = `[setExp.disable()]`
         //  Handle if module already disabled before the action.
-        if (!this.primaryConfig.value) return reply(this.locale.SETLEVELUPMESSAGE.ALREADY_DISABLED, {status: `warn`})
+        if (!this.primaryConfig.value) return reply(this.locale.SETEXP.ALREADY_DISABLED, {status: `warn`})
         //  Update configs
         await this.bot.db.updateGuildConfiguration({
             configCode: this.primaryConfigID,
@@ -110,16 +115,16 @@ class SetLevelupMessage extends Command {
             cacheTo: this.guildConfigurations
         })
         this.logger.info(`${fn} ${this.primaryConfigID} for GUILD_ID:${this.message.guild.id} has been disabled.`)
-        return reply(this.locale.SETLEVELUPMESSAGE.SUCCESSFULLY_DISABLED, {status: `success`})
+        return reply(this.locale.SETEXP.SUCCESSFULLY_DISABLED, {status: `success`})
     }
 }
 
 module.exports.help = {
-    start: SetLevelupMessage,
-    name: `setLevelupMessage`,
-    aliases: [`setlevelupmsg`, `setlvlupmsg`, `setlvlupmessage`, `setlevelupmessage`],
-    description: `Enable or disable level-up message module for this guild`,
-    usage: `setlvlupmsg <Enable/Disable>`,
+    start: SetExp,
+    name: `setExp`,
+    aliases: [`setexp`, `setexperience`, `setxp`],
+    description: `Enable or disable EXP Leveling System for this guild`,
+    usage: `setexp <Enable/Disable>`,
     group: `Setting`,
     permissionLevel: 3,
     multiUser: false
