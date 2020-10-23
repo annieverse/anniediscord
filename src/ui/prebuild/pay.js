56e4981cf087ff4b4868bd28aa87aa42f65b641e
Naph <nightcore.raven07@gmail.com>
2020-10-24 01:36:43 +0700

    refactor(pay): shared ui, reduced tax and lvl requirement

--- src/ui/prebuild/pay.js
+++ src/ui/prebuild/pay.js
@@ -2,8 +2,6 @@ const Cards = require(`../../ui/components/cards`)
 const commanifier = require(`../../utils/commanifier`)
 const urlToBuffer = require(`../../utils/urlToBuffer`)
 const loadAsset = require(`../../utils/loadAsset`)
-const Color = require(`color`)
-
 class UI {
 	/**
 	 * Pay UI Builder.
@@ -21,9 +19,9 @@ class UI {
 		//  Initialize framework
 		let card = await new Cards({
 			width: 320,
-			height: 310,
-			justify: `center`,
-			align: `center`,
+			height: 210,
+			justify: `left`,
+			align: `left`,
 			theme: this.user.usedTheme.alias
 		})
 		//  Base card
@@ -32,16 +30,30 @@ class UI {
 		.addCover({ img: await loadAsset(this.user.usedCover.alias), gradient: true })
 		await card.addContent({
 			avatar: await urlToBuffer(this.user.user.displayAvatarURL({format: `png`, dynamic: false})),
-			avatarRadius: 12,
-			justify: `center`,
-			marginTop: 60
+			avatarRadius: 7,
+			marginTop: 65,
+			marginLeft: 80,
+			inline: true
 		})
 		//  Balance Author
-		card.addTitle({main: this.user.user.username, size: 12, marginTop: 30})
-		.addTitle({main: `will receive ...`, size: 9, marginTop: 20})
-		//  Amount to send
-		.addTitle({main: commanifier(this.amountToSend), size: 30, marginTop: 80})
-		.addTitle({main: `artcoins?`, size: 9, marginTop: 25})
+		card.addTitle({main: this.user.user.username + `,`, size: 12, marginLeft: 60, marginTop: 60, fontWeight: `bold`})
+		.addTitle({main: `will receive ...`, size: 9, marginLeft: 70, marginTop: 15})
+		//  Artcoins visual
+		const amountTextLength = commanifier(this.amountToSend).length
+		const imageRadius = 50
+		await card.addContent({
+			img: await loadAsset(`artcoins`),
+			justify: `center`,
+			align: `center`,
+			marginLeft: (0-(imageRadius/2)) - amountTextLength*12 + (amountTextLength >= 8 ? 20 : 0),
+			imgDx: imageRadius,
+			imgDy: imageRadius,
+			marginTop: 20,
+			inline: true
+		})
+		//  Amount to get
+		const dynamicX = amountTextLength <= 3 ? 20 + amountTextLength*2 : 10 + amountTextLength*2
+		card.addTitle({main: commanifier(this.amountToSend), size: 30, marginTop: 60, marginLeft: dynamicX,justify: `center`, align: `center`})
 		// Finalize
 		.ready()
 		return card.getBuffer()
