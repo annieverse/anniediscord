9a0225defeef0557abb65e4240617b8e5a0a637f
Naph <nightcore.raven07@gmail.com>
2020-11-21 04:09:33 +0700

    refactor(roleDelete): ommits from cache and db when registered ranks or welcomer roles got deleted

--- src/events/roleDelete.js
+++ src/events/roleDelete.js
@@ -6,4 +6,42 @@ module.exports = async (bot, role, configs) => {
         guild: role.guild
     }
     if (configs.get(`LOGS_MODULE`).value) new bot.logSystem(metadata)
+    //  Re-validate registered ranks
+    const registeredRanks = configs.get(`RANKS_LIST`)
+	if (registeredRanks.value.length > 0) {
+		for (let i=0; i<registeredRanks.value.length; i++) {
+			const rank = registeredRanks.value[i]
+			//  If the deleted role is one of the registered rank, the ommits from cache and database
+			if (role.id === rank.ROLE) {
+				const newRegisteredRanks = registeredRanks.value.filter(node => node.ROLE != role.id)
+		        await bot.db.updateGuildConfiguration({
+		            configCode: `RANKS_LIST`,
+		            customizedParameter: newRegisteredRanks,
+		            guild: role.guild,
+		            setByUserId: registeredRanks.setByUserId,
+		            cacheTo: configs
+		        })
+		        bot.logger.info(`[Event.roleDelete] Delete registered rank(LV:${rank.LEVEL}) ROLE_ID:${role.id} from GUILD_ID:${role.guild.id}`)
+			}
+		}
+	}
+	//  Re-validate registered welcomer roles
+	const registeredWelcomerRoles = configs.get(`WELCOMER_ROLES`)
+	if (registeredWelcomerRoles.value.length > 0) {
+		for (let i=0; i<registeredWelcomerRoles.value.length; i++) {
+			const welcomerRole = registeredWelcomerRoles.value[i]
+			//  If the deleted role is one of the welcomer role, the ommits from cache and database
+			if (role.id === welcomerRole) {
+				const newWelcomerRoles = registeredWelcomerRoles.value.filter(node => node != role.id)
+		        await bot.db.updateGuildConfiguration({
+		            configCode: `WELCOMER_ROLES`,
+		            customizedParameter: newWelcomerRoles,
+		            guild: role.guild,
+		            setByUserId: registeredWelcomerRoles.setByUserId,
+		            cacheTo: configs
+		        })
+		        bot.logger.info(`[Event.roleDelete] Delete registered welcomer ROLE_ID:${role.id} from GUILD_ID:${role.guild.id}`)
+			}
+		}
+	}
 }
