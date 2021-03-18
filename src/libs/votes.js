5e7520c8dda19dba575a238902e44705b623be72
Naph <nightcore.raven07@gmail.com>
2021-03-18 22:55:51 +0700

    fix(vote): added emoji handler.

--- src/libs/votes.js
+++ src/libs/votes.js
@@ -1,5 +1,6 @@
 const DBL = require(`dblapi.js`)
 const commanifier = require(`../utils/commanifier`)
+const emoji = require(`../utils/emojiFetch`)
 /**
  * top.gg votes manager
  * only need current client instance as the dependency.
@@ -23,15 +24,16 @@ class Votes {
 	    this.bot.registerNode(dbl, `dbl`)
 		dbl.webhook.on(`ready`, hook => this.logger.info(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`))
 		dbl.webhook.on(`vote`, async vote => {
-			this.logger.info(`USER_ID: ${vote.user} just voted!`)
-			dbl.postStats((await this.bot.shard.fetchClientValues(`guilds.cache.size`)).reduce((acc, guildCount) => acc + guildCount, 0))
-			this.bot.db.updateInventory({itemId: 52, userId: vote.user, value: this.reward, distributeMultiAccounts: true})
+			this.logger.info(`USER_ID:${vote.user} SHARD_ID:${this.bot.shard.ids[0]} just voted!`)
+			const guildfromShards = await this.bot.shard.fetchClientValues(`guilds.cache.size`)
+			dbl.postStats(guildfromShards.reduce((acc, guildCount) => acc + guildCount, 0))
 			const user = await this.bot.users.fetch(vote.user)
 			try {
-				user.send(`**Thanks for the voting, ${user.username}!** I've sent ${this.bot.emojis.cache.find(node => node.name === `artcoins`)}**${commanifier(this.reward)}** to your inventory as the reward!\nYou can check it by typing \`${this.prefix}bal\` or \`${this.prefix}inventory\` in the server.`)
+				this.bot.db.updateInventory({itemId: 52, userId: vote.user, value: this.reward, distributeMultiAccounts: true})
+				user.send(`**Thanks for the voting, ${user.username}!** I've sent ${await emoji(`artcoins`, this.bot)}**${commanifier(this.reward)}** to your inventory as the reward!\nYou can check it by typing \`${this.prefix}bal\` or \`${this.prefix}inventory\` in the server.`)
 			}
 			catch (e) {
-				this.logger.warn(`${fn} has failed to send reward notification due to locked DM for USER_ID: ${vote.user}`)
+				this.logger.warn(`${fn} has failed to send reward notification due to locked DM for USER_ID: ${vote.user} > ${e.stack}`)
 			}
 		})
 	}
---
+++ src/utils/emojiFetch.js
@@ -0,0 +1,37 @@
+const Discord = require(`discord.js`)
+/**
+ * Fetch global emojis through shard broadcast eval
+ * @param {string} keyword
+ * @return {object}
+ */
+const broadcastScript = (keyword) => {	
+    const temp = this.emojis.cache.get(keyword) || this.emojis.cache.find(e => e.name === keyword)
+    if (!temp) return null
+    // Clone the object because it is modified right after, so as to not affect the cache in client.emojis
+    const emoji = Object.assign({}, temp)
+    // Circular references can't be returned outside of eval, so change it to the id
+    if (emoji.guild) emoji.guild = emoji.guild.id
+    // A new object will be constructed, so simulate raw data by adding this property back
+    emoji.require_colons = emoji.requiresColons
+    return emoji
+}
+
+/**
+ *  Fetching emojis across all shards.
+ *  @param {string} emojiKeyword emoji's ID/Snowflake/Name
+ *  @param {object} client Current client instance
+ *  @return {string|Discord.Emoji} 
+ */
+emojiFetch = async (emojiKeyword, client) => {
+    const runScript = await client.shard.broadcastEval(`(${broadcastScript}).call(this, '${emojiKeyword}')`)
+    const findEmoji = runScript.find(e => e)
+    if (!findEmoji) return `(???)`
+    const raw = await client.api.guilds(findEmoji.guild).get()
+    const guild = new Discord.Guild(client, raw)
+    return new Discord.GuildEmoji(client, findEmoji, guild)
+}
+
+module.exports = emojiFetch
+
+
+
