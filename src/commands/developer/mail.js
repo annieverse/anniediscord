6d52b36eb6b4dc2d808fff4040da0dbb1946a10b
Naph <nightcore.raven07@gmail.com>
2021-03-25 17:10:05 +0700

    feat(devkit): internal mailling system.

---
+++ src/commands/developer/mail.js
@@ -0,0 +1,56 @@
+const Command = require(`../../libs/commands`)
+/**
+ * Allows developer to send a private message to reachable user.
+ * @author klerikdust
+ */
+class Mail extends Command {
+
+    /**
+     * @param {external:CommandComponents} Stacks refer to Commands Controller.
+     */
+	constructor(Stacks) {
+		super(Stacks)
+	}
+
+    /**
+     * Running command workflow
+     * @param {PistachioMethods} Object pull any pistachio's methods in here.
+     */
+	async execute({ reply, bot:{db}, emoji, commanifier }) {
+		await this.requestUserMetadata(1)
+		if (!this.user) return reply(`Sadly, the user is unreachable`)
+		//  Handle if user doesn't specify any arg
+		if (!this.fullArgs) return reply(`Who you want me to send DM to? ${await emoji(`AnnieThinking`)}`)
+		const confirmation = await reply(`I'm going to send **${this.user.master}** the following message.\n\`\`\`\n${this.fullArgs}\n\`\`\``)
+		await this.addConfirmationButton(`send_mail`, confirmation)
+ 		return this.confirmationButtons.get(`send_mail`).on(`collect`, async r => {
+			//  Handle cancellation
+			if (this.isCancelled(r)) return reply(this.locale.ACTION_CANCELLED, {
+				socket: {emoji: await emoji(`781954016271138857`)}
+			})
+			try {
+				await reply(this.fullArgs, {
+					field: this.user.master,
+					footer: `This system message is sent by the developer.`
+				})
+				this.finalizeConfirmation(r)
+				return reply(`The mail is successfully sent!`)
+			}
+			catch(e) {
+				this.logger.warn(`[DEVKIT_DM] USER_ID:${this.user.master.id} > ${e.message}`)
+				return reply(`Unfortunately I can't forward the email due to locked DM.`)
+			}
+		})
+	}
+}
+
+module.exports.help = {
+	start: Mail,
+	name: `mail`,
+	aliases: [`mail`, `dm`],
+	description: `Allows developer to send a private message to reachable user`,
+	usage: `<user> <message>`,
+	group: `Developer`,
+	permissionLevel: 4,
+	multiUser: true
+}
