7b7b4eda991479c36b112ae9e2c309aa24646200
Nabil <nabilbasyaiban@outlook.com>
2021-05-17 01:14:12 +0700

    refactor(permissions): simplified permission controller with configless dependency.

--- src/libs/permissions.js
+++ src/libs/permissions.js
@@ -1,22 +1,36 @@
-const privilege = require(`../config/permissions`)
 /**
- * Fetch user's permission level
+ * Decide target user's permissions level.
  * @param {object} [message={}] Target message's instance.
  * @param {string} [userId=``] Target user.
- * @returns {object}
+ * @return {object}
  */
 module.exports = (message={}, userId={}) => {
-    // Check for developer privileges
-    if (privilege[4].accounts.includes(userId)) return privilege[4]
+    //  Developer privileges
+    if ([`230034968515051520`, `277266191540551680`].includes(userId)) return {
+        level: 4,
+        name: `Developer`,
+        description: `System developer privileges`,
+        permissionString: `ADMINISTRATOR`
+    }
     // User without developer privileges in dm interface will be automatically assigned as a regular user.
     if (message.channel.type === `dm`) return privilege[0]
     const member = message.member
-    const descendingPrivileges = Object.keys(privilege).sort((a, b) => b - a)
-    for (let i=0; i<descendingPrivileges.length; i++) {
-        const pvObj = privilege[descendingPrivileges[i]]
-        if (pvObj.level === 4) continue
-        if (member.hasPermission(pvObj.permissionString)) return pvObj
+    //  Server admin
+    if (member.hasPermission(`ADMINISTRATOR`)) return {
+        level: 3,
+        name: `Administrator`,
+        description: `Server's super user`
     }
-    // Returns as regular user if no level is matched
-    return privilege[0]
+    //  Moderator
+    if (member.hasPermission(`MANAGE_ROLES`)) return { 
+        level: 2,
+        name: `Moderator`,
+        description: `Server's manager with moderation capabilities`
+    }
+    //  If no special privileges are match, fall back to regular-user privilege
+    return {
+        level: 0,
+        name: `User`,
+        description: `Regular user`,
+    }
 }
