b8d58c200c21122b4496afdd825495fd35cf3b85
Nabil <nabilbasyaiban@outlook.com>
2021-05-31 14:51:31 +0700

    perf(AutoResponder): switch to in-memory cd instead of redis.

--- src/controllers/autoResponder.js
+++ src/controllers/autoResponder.js
@@ -5,19 +5,23 @@
  * @return {object|winston}
  */
 module.exports = async (client, message) => {
-    const controllerId = `[Controller.AutoResponder]`
     //  Reject if guild does not have any registered AR.
     const ars = await client.db.getAutoResponders(message.guild.id)
     if (ars.length <= 0) return 
     //  Reject if context doesn't match with guild's any registered ARs.
     const foundArs = ars.filter(ar => ar.trigger === message.content)
     if (foundArs.length <= 0) return 
-    //  Handle if AR still cooldown.
+    //  15s cooldown to prevent spam
     const { ar_id, response } = foundArs[0]
+    const cooldown = 15 // in seconds
     const ARCooldownId = `AR_${ar_id}@${message.guild.id}`
-    if (await client.isCooldown(ARCooldownId)) return
-    client.setCooldown(ARCooldownId, 15)
+    if (client.cooldowns.has(ARCooldownId)) {
+        const recentCooldown = client.cooldowns.get(ARCooldownId)
+        const diff = cooldown - ((Date.now() / recentCooldown) / 1000)
+        if (diff > 0) return
+    }
+    client.cooldowns.set(ARCooldownId, Datee.now())
     //  Send response and handle incase fail to send the response.
     return message.channel.send(response)
-    .catch(e => client.logger.warn(`${controllerId} has failed to send AR_${ar_id} response in GUILD_ID:${message.guild.id} due to > ${e.message}`))
+    .catch(e => e)
 }
