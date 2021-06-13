e443d5bfe85031a48549711fb915e5361b16666d
Nabil <nabilbasyaiban@outlook.com>
2021-06-13 18:49:10 +0700

    fix(user): incomplete data registers for new user.

--- src/libs/user.js
+++ src/libs/user.js
@@ -128,25 +128,14 @@ class User {
     /**
      * Requesting standard user metadata from Discord API and/or Database
      * @param {collection} [user] collection of user's metadata
-	 * @param {number} [dataLevel=1] set to `1` to return standard user's discord structure with custom locale. Set to `2` to have an additional
-	 * properties such as inventory, exp, level, reputations, etc/
      * @returns {UserMetadataObject}
      */
-    async requestMetadata(user, dataLevel=1) {
+    async requestMetadata(user) {
 		const fn = `[User.requestMetadata()]`
 		const db = this.bot.db
 		//  Handle if user object isn't valid
 		if (!user.id || typeof user !== `object`) throw new TypeError(`${fn} parameter 'user' should be a valid collection of user metadata.`)
 		try {
-			/** --------------------------------------------------------------------
-			 *  DATA-BLOCK LEVEL 1
-			 *  --------------------------------------------------------------------
-			 *  Only consists of discord properties data + user's customized locale.
-			 */
-			let getUserLocale = await db.getUserLocale(user.id)
-			const lang = getUserLocale ? getUserLocale.lang : `en`
-			if (dataLevel <= 1) return {master:user, lang:lang}
-
 			/** --------------------------------------------------------------------
 			 *  DATA-BLOCK LEVEL 2
 			 *  --------------------------------------------------------------------
@@ -155,7 +144,7 @@ class User {
 			//  Basic data such as saved username, language, user_id and registered date
 			const main = await db.getUser(user.id)
 			//  User's reputations data
-			const reputations = await db.getUserReputations(user.id, this.message.guild.id)
+			const reputations = await db.getUserReputation(user.id, this.message.guild.id)
 			//  User's dailies, streak and stuff
 			const dailies = await db.getUserDailies(user.id, this.message.guild.id)
 			//  User's relationship trees.
@@ -254,7 +243,6 @@ class User {
 			return {
 				master:user,
 				main:main,
-				lang:lang,
 				reputations:reputations,
 				dailies:dailies,
 				relationships:relationships,
