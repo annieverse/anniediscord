43c06374b0052776619362d30229f6517fc03deb
Nabil <nabilbasyaiban@outlook.com>
2021-06-19 15:51:46 +0700

    chore(itemEffect): method progression.

--- src/libs/itemEffects.js
+++ src/libs/itemEffects.js
@@ -18,6 +18,11 @@ class itemEffects {
          * @type {Message}
          */
         this.message = message
+        /**
+         * Instance identifier
+         * @type {string}
+         */
+        this.instanceId = `ITEM_EFFECT_${message.guild.id}:${message.author.id}`
     }
     
     /**
@@ -30,9 +35,45 @@ class itemEffects {
             `REMOVE_ROLE`,
             `ADD_ITEM`,
             `REMOVE_ITEM`,
+            `ADD_EXP`,
+            `SUBTRACT_EXP`,
             `EXP_BUFF`,
             `ARTCOINS_BUFF`,
         ]
     }
+
+    /**
+     *  ----------------------------
+     *  DEFINED FLOW FOR EACH BUFF EFFECT
+     *  ---------------------------
+     */
+    
+    /**
+     * Giving out role
+     * @param {string|object} roleIds Pool of role ids
+     * return {void}
+     */
+    addRole(roleIds) {
+        //  Skip addition if user already has the role
+        if (this.message.member.roles.cache.has(roleId)) return
+        this.message.member.roles.add(roleIds)
+        .catch(e => this.client.logger.warn(`${this.instanceId} <ADD_ROLE_FAIL> ${e.stack}`))
+    }
+
+    /**
+     * Extract/revoke out role
+     * @param {string|object} roleIds Pool of role ids
+     * return {void}
+     */
+    removeRole(roleIds) {
+        //  Skip addition if user already has the role
+        if (this.message.member.roles.cache.has(roleId)) return
+        this.message.member.roles.remove(roleIds)
+        .catch(e => this.client.logger.warn(`${this.instanceId} <REMOVE_ROLE_FAIL> ${e.stack}`))
+    }
+
+    /**
+     * Adding specific item to user's inventory.
+     */
 }
 module.exports = itemEffects
