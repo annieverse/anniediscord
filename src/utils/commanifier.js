510dbef781bda53cd1ff549621f88537ef41bac3
Nabil <nabilbasyaiban@outlook.com>
2021-07-18 16:25:43 +0700

    fix(commanifier): decimal numbers transformed into comma-separated number.

--- src/utils/commanifier.js
+++ src/utils/commanifier.js
@@ -1,9 +1,11 @@
 /**
  *  Add comma separator on given number. Only applies to number above 3 digits.
  *  @param {number} [number=0] target number to be parsed from
+ *  @param {boolean} [roundUp=true] Transform targer number into a rounded number before gets replaced with commas. Optional.
  *  @returns {string}
  */
-const commanifier = (number=0) => {
+const commanifier = (number=0, roundUp=true) => {
+    if (roundUp) number = Math.round(number)
 	return number ? number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, `,`) : 0
 }
 
