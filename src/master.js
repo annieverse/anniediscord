37de5dc6fa812d65060126684c919fabaf8ed026
Andrew <thefryingpan2021@gmail.com>
2022-06-25 20:15:02 -0400

    chore(fix): vote webhook

--- src/master.js
+++ src/master.js
@@ -30,44 +30,14 @@ module.exports = function masterShard() {
     //  Spawn shard sequentially with 30 seconds interval. 
     //  Will send timeout warn in 2 minutes.
     manager.spawn(`auto`, 30000, 60000*2)
-    //  Vote event
-    const rewardDistribution = async (userId) => {
-        this.users.fetch(userId)
-		.then(async user => {
-			//  Only perform on SHARD_ID:0
-			if (this.shard.ids[0] === 0) {
-				this.dblApi.postStats({
-					serverCount: this.guilds.cache.size,
-					shardId: this.shard.ids[0],
-					shardCount: this.options.shardCount
-				})
-				this.db.updateInventory({
-					itemId: 52, 
-					userId: userId, 
-					value: 5000, 
-					distributeMultiAccounts: true
-				})
-				const artcoinsEmoji = await this.getEmoji(`artcoins`)
-				user.send(`**Thanks for the voting, ${user.username}!** I've sent ${artcoinsEmoji}**5,000** to your inventory as the reward!`)
-				.catch(e => this.logger.warn(`FAIL to DM USER_ID:${userId} on SHARD_ID:${this.shard.ids[0]} > ${e.message}`))
-				this.logger.info(`Vote reward successfully sent to USER_ID:${userId}`)
-			}
-		})
-		.catch(e => {
-			this.logger.warn(`FAIL to find USER_ID:${userId} on SHARD_ID:${this.shard.ids[0]} so no reward given > ${e.message}`)
-		})
-    }
-
     const wh = new Webhook(process.env.DBLWEBHOOK_AUTH)
-    server.post(`/dblwebhook`, wh.listener(), (req, res) => {
-        if (!req.vote) {
+    //  Send shard count to DBL webhook.
+	server.post(`/dblwebhook`, wh.listener((vote, req, res) => {
+		if (!req.vote) {
             res.status(200).send({ message: `Endpoint successfully tested` })
         }
         const userId = req.vote.user
         logger.info(`USER_ID:${userId} just voted!`)
-		function getUser(userId) {
-			return this.users.fetch(userId)
-		}
         manager.broadcastEval( c => {
             c.users.fetch(userId)
 		.then(async user => {
@@ -95,7 +65,7 @@ module.exports = function masterShard() {
 		})
         	})
         res.status(200).send({ message: `Vote data successfully received.` })
-    })
+    }))
     const port = process.env.PORT || 3000
     server.listen(port, () => logger.info(`<LISTEN> PORT:${port}`))
 }
