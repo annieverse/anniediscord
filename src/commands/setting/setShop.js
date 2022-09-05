41f57757515660a8251f1a4b891eaa4d6b362a05
Andrew <thefryingpan2021@gmail.com>
2022-09-05 00:55:42 -0400

    fix(shop): add item fails halfway through

--- src/commands/setting/setShop.js
+++ src/commands/setting/setShop.js
@@ -257,7 +257,7 @@ module.exports = {
             )
         let responseMessageContent = {
             instructions: `Please click the button of the value you would like to enter`,
-            buffs: `**Will it grants any bonus effect once used? (You can add up to 3 bonus effects per item)**\n♡ Once in the popup after hitting the \`buffs?\` button do one of the following\n♡ Type **\`addrole <roles>\`** to get specified roles once used.\n♡ Type **\`removerole <roles>\`** to remove specified roles from user once used.\n♡ Type **\`additem <amount> <itemName/itemId>\`** to receive specified items once used.\n♡ Type **\`removeitem <amount> <itemName/itemId>\`** to remove specified items from user's inventory.\n♡ Type **\`expboost <percentage> <duration>\`** To give exp boost to the user for specified amount of time.\n♡ Type **\`acboost <percentage> <duration>\`** to give artcoins boost to the user for specified amount of time.`,
+            buffsoptions: `**Will it grants any bonus effect once used? (You can add up to 3 bonus effects per item)**\n♡ Once in the popup after hitting the \`buffs?\` button do one of the following\n♡ Type **\`addrole <roles>\`** to get specified roles once used.\n♡ Type **\`removerole <roles>\`** to remove specified roles from user once used.\n♡ Type **\`additem <amount> <itemName/itemId>\`** to receive specified items once used.\n♡ Type **\`removeitem <amount> <itemName/itemId>\`** to remove specified items from user's inventory.\n♡ Type **\`expboost <percentage> <duration>\`** To give exp boost to the user for specified amount of time.\n♡ Type **\`acboost <percentage> <duration>\`** to give artcoins boost to the user for specified amount of time.`,
             start: `\n╰☆～**Name ::** ${metadata.name}\n╰☆～**Description ::** ${metadata.description}\n╰☆～**Price ::** ${await client.getEmoji(`artcoins`)}${commanifier(metadata.price)} @pcs\n╰☆～**Can be traded ::** ${metadata.tradeable == `y` ? `yes` : `no`}\n╰☆～**My response after the item is used ::** ${metadata.useMessage === `~` ? `default` : metadata.useMessage}`,
         }
         
@@ -384,7 +384,7 @@ module.exports = {
                     followUp: true
                 })
                 //  Role update buff
-                const clientRole = message.guild.me.roles.highest
+                const clientRole = interaction.guild.members.me.roles.highest
                 if ([`addrole`, `removerole`].includes(params[0])) {
                     let res = []
                     let roleNames = ``
@@ -418,11 +418,13 @@ module.exports = {
                 if ([`additem`, `removeitem`].includes(params[0])) {
                     const amount = trueInt(params[1])
                     if (!amount) return reply.send(locale.SETSHOP.ADD_BUFF_INVALID_ITEM_AMOUNT, {
-                        deleteIn: 5
+                        deleteIn: 5,
+                        followUp:true
                     })
                     const targetItem = await client.db.getItem(params.slice(2).join(` `))
                     if (!targetItem) return reply.send(locale.SETSHOP.ADD_BUFF_INVALID_TARGET_ITEM, {
-                        deleteIn: 5
+                        deleteIn: 5,
+                        followUp:true
                     })
                     const isItemAddition = params[0] === `additem`
                     buffs.push({
@@ -440,11 +442,13 @@ module.exports = {
                 if ([`expboost`, `acboost`].includes(params[0])) {
                     const multiplier = params[1].replace(/[^0-9a-z-A-Z ]/g, ``)
                     if (!multiplier) return reply.send(locale.SETSHOP.ADD_BUFF_INVALID_MULTIPLIER, {
-                        deleteIn: 5
+                        deleteIn: 5,
+                        followUp:true
                     })
                     const duration = ms(params.slice(2).join(` `))
                     if (!duration) return reply.send(locale.SETSHOP.ADD_BUFF_INVALID_DURATION, {
-                        deleteIn: 5
+                        deleteIn: 5,
+                        followUp:true
                     })
                     const isExpBuff = params[0] === `expboost`
                     buffs.push({
