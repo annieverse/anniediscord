9030144cc41dc4f4449eb52234fa92ba83123efc
Andrew <thefryingpan2021@gmail.com>
2022-10-05 18:44:14 -0400

    chore(dev): optimize who's local bot is in use

--- src/commands/applicationCommandsLoader.js
+++ src/commands/applicationCommandsLoader.js
@@ -29,51 +29,44 @@ module.exports = function applicationCommandLoader({
 
     const NODE_ENVIRONMENT_CLIENT_ID = process.env.NODE_ENV === `production` ? `501461775821176832` : process.env.NODE_ENV === `production_beta` ? `788001359349547039` : null
 
-    if (!NODE_ENVIRONMENT_CLIENT_ID && process.env.NODE_ENV !== `development`) {return process.exit(9)}
+    if (!NODE_ENVIRONMENT_CLIENT_ID && process.env.NODE_ENV !== `development`) { return process.exit(9) }
 
-    (async() => {
+    (async () => {
         try {
             logger.info(`Started refreshing application (/) commands.`)
             if (process.env.NODE_ENV === `production` || process.env.NODE_ENV === `production_beta`) {
                 await rest.put(
                     Routes.applicationCommands(NODE_ENVIRONMENT_CLIENT_ID), {
-                        body: applicationCommands
-                    },
+                    body: applicationCommands
+                },
                 )
             } else {
-                /**
-                 * For Pan's local bot use only
-                 */
-                /*
-                // Annie support server
-                await rest.put(
-                    Routes.applicationGuildCommands(`514688969355821077`, `577121315480272908`), {
+                if (process.env.NODE_DEV_CLIENT === `PAN`) {
+                    /**
+                     * For Pan's local bot use only
+                     */
+                    // Annie support server
+                    await rest.put(
+                        Routes.applicationGuildCommands(`514688969355821077`, `577121315480272908`), {
                         body: applicationCommands
                     },
-                ) 
-                // Pan's test server
-                await rest.put(
-                    Routes.applicationGuildCommands(`514688969355821077`, `597171669550759936`), {
+                    )
+                    // Pan's test server
+                    await rest.put(
+                        Routes.applicationGuildCommands(`514688969355821077`, `597171669550759936`), {
                         body: applicationCommands
                     },
-                ) 
-                /**
-                 * For Annie's local bot use only
-                 */
-                /* await rest.put(
-                    Routes.applicationGuildCommands(`501461775821176832`, `577121315480272908`), {
+                    )
+                } else if (process.env.NODE_DEV_CLIENT === `NAPH`) {
+                    /**
+                     * For Naph's local bot use only
+                     */
+                    await rest.put(
+                        Routes.applicationGuildCommands(`581546189925646350`, `577121315480272908`), {
                         body: applicationCommands
                     },
-                ) */
-                
-                /**
-                 * For Naph's local bot use only
-                 */
-                await rest.put(
-                    Routes.applicationGuildCommands(`581546189925646350`, `577121315480272908`), {
-                        body: applicationCommands
-                    },
-                ) 
+                    )
+                }
             }
             logger.info(`Successfully reloaded application (/) commands. ${applicationCommands.size} Commands`)
         } catch (error) {
