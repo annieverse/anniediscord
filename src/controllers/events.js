2fbad1d4777925b4793264c4e2cb6ec9cf66b6d1
Andrew <thefryingpan2021@gmail.com>
2023-01-22 16:27:33 -0500

    chore(variables): using enums

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,31 +1,32 @@
 const reqEvent = (event) => require(`../events/${event}.js`)
+const {Events} = require(`discord.js`)
     /**
      * Handles events in the current node
      * @param {client} Annie Current bot instance.
      * @return {void}
      */
 module.exports = function eventsController(annie) {
-    annie.once(`ready`, () => reqEvent(`ready`)(annie))
-    annie.on(`error`, (e) => reqEvent(`error`)(annie, e))
-    annie.on(`messageCreate`, (message) => reqEvent(`messageCreate`)(annie, message))
-    annie.on(`guildCreate`, (guild) => reqEvent(`guildCreate`)(annie, guild))
-    annie.on(`guildDelete`, (guild) => reqEvent(`guildDelete`)(annie, guild))
-    annie.on(`interactionCreate`, (interaction) => reqEvent(`interactionCreate`)(annie, interaction))
-    annie.on(`debug`,l => annie.logger.info(l))
-    annie.on(`warn`,l => annie.logger.info(l))
-    annie.on(`shardReady`,(id, unavailableGuilds) => reqEvent(`shardReady`)(annie, id, unavailableGuilds))
+    annie.once(Events.ClientReady, () => reqEvent(`ready`)(annie))
+    annie.on(Events.Error, (e) => reqEvent(`error`)(annie, e))
+    annie.on(Events.MessageCreate, (message) => reqEvent(`messageCreate`)(annie, message))
+    annie.on(Events.GuildCreate, (guild) => reqEvent(`guildCreate`)(annie, guild))
+    annie.on(Events.GuildDelete, (guild) => reqEvent(`guildDelete`)(annie, guild))
+    annie.on(Events.InteractionCreate, (interaction) => reqEvent(`interactionCreate`)(annie, interaction))
+    annie.on(Events.Debug,l => annie.logger.info(l))
+    annie.on(Events.Warn,l => annie.logger.info(l))
+    annie.on(Events.ShardReady,(id, unavailableGuilds) => reqEvent(`shardReady`)(annie, id, unavailableGuilds))
         //  Events below this point is only available in the production
     if (annie.dev) return
-    annie.on(`messageDelete`, (message) => reqEvent(`messageDelete`)(annie, message))
-    annie.on(`messageDeleteBulk`, (messages, channel) => reqEvent(`messageDeleteBulk`)(annie, messages, channel))
-    annie.on(`roleCreate`, (role) => reqEvent(`roleCreate`)(annie, role))
-    annie.on(`roleDelete`, (role) => reqEvent(`roleDelete`)(annie, role))
-    annie.on(`emojiCreate`, (emoji) => reqEvent(`emojiCreate`)(annie, emoji))
-    annie.on(`emojiDelete`, (emoji) => reqEvent(`emojiDelete`)(annie, emoji))
-    annie.on(`channelDelete`, (channel) => reqEvent(`channelDelete`)(annie, channel))
-    annie.on(`channelCreate`, (channel) => reqEvent(`channelCreate`)(annie, channel))
-    annie.on(`guildBanAdd`, (guild, user) => reqEvent(`guildBanAdd`)(annie, guild, user))
-    annie.on(`guildBanRemove`, (guild, user) => reqEvent(`guildBanRemove`)(annie, guild, user))
-    annie.on(`guildMemberAdd`, (member) => reqEvent(`guildMemberAdd`)(annie, member))
-    annie.on(`guildMemberUpdate`, (oldMember,newMember) => reqEvent(`guildMemberUpdate`)(annie, oldMember,newMember))
+    annie.on(Events.MessageDelete, (message) => reqEvent(`messageDelete`)(annie, message))
+    annie.on(Events.MessageBulkDelete, (messages, channel) => reqEvent(`messageDeleteBulk`)(annie, messages, channel))
+    annie.on(Events.GuildRoleCreate, (role) => reqEvent(`roleCreate`)(annie, role))
+    annie.on(Events.GuildRoleDelete, (role) => reqEvent(`roleDelete`)(annie, role))
+    annie.on(Events.GuildEmojiCreate, (emoji) => reqEvent(`emojiCreate`)(annie, emoji))
+    annie.on(Events.GuildEmojiDelete, (emoji) => reqEvent(`emojiDelete`)(annie, emoji))
+    annie.on(Events.ChannelDelete, (channel) => reqEvent(`channelDelete`)(annie, channel))
+    annie.on(Events.ChannelCreate, (channel) => reqEvent(`channelCreate`)(annie, channel))
+    annie.on(Events.GuildBanAdd, (guild, user) => reqEvent(`guildBanAdd`)(annie, guild, user))
+    annie.on(Events.GuildBanRemove, (guild, user) => reqEvent(`guildBanRemove`)(annie, guild, user))
+    annie.on(Events.GuildMemberAdd, (member) => reqEvent(`guildMemberAdd`)(annie, member))
+    annie.on(Events.GuildMemberUpdate, (oldMember,newMember) => reqEvent(`guildMemberUpdate`)(annie, oldMember,newMember))
 }
