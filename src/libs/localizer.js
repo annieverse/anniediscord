4a7573c7075b4d7a195e75be85611ef0e2775d7e
Nabil <nightcore.raven07@gmail.com>
2023-05-16 13:29:58 +0700

    test(localizer): linter.

--- src/libs/localizer.js
+++ src/libs/localizer.js
@@ -11,58 +11,58 @@ class Localizer {
      */
     constructor(path = `./src/locales/`, defaultLang = `en`) {
       // The name of the function for logging.
-      this.fn = `[LOCALIZER]`;
+      this.fn = `[LOCALIZER]`
   
       // The path of the target locale directory.
-      this.path = path;
+      this.path = path
   
       // The pool of available locales.
-      this.localesPool = {};
+      this.localesPool = {}
 
       // The codes pool of available locales.
-      this.localeCodesPool = [];
+      this.localeCodesPool = []
   
       // The list of files in the target directory.
-      this.src = readdirSync(path);
+      this.src = readdirSync(path)
 
       // Lang to be used by default
       this.defaultLang = defaultLang
   
       // Filter the list of files to only include .json files.
-      const locales = this.src.filter((loc) => loc.endsWith(`json`));
+      const locales = this.src.filter((loc) => loc.endsWith(`json`))
   
       // Throw an error if there are no locales available.
       if (!locales.length) {
-        logger.error(`${this.fn} can't find any locales in '${path}'`);
+        logger.error(`${this.fn} can't find any locales in '${path}'`)
       }
   
       // Load the default locale first
-      const defaultLocale = locales.find((file) => file.replace(/.json/, '') === this.defaultLang);
+      const defaultLocale = locales.find((file) => file.replace(/.json/, ``) === this.defaultLang)
       if (!defaultLocale) {
-        logger.error(`${this.fn} default locale '${this.defaultLang}' does not exist`);
+        logger.error(`${this.fn} default locale '${this.defaultLang}' does not exist`)
       }
 
-      const defaultLocaleCode = defaultLocale.replace(/.json/, '');
-      this.localeCodesPool.push(defaultLocaleCode);
-      this.localesPool[defaultLocaleCode] = require(`../locales/${defaultLocale}`);
+      const defaultLocaleCode = defaultLocale.replace(/.json/, ``)
+      this.localeCodesPool.push(defaultLocaleCode)
+      this.localesPool[defaultLocaleCode] = require(`../locales/${defaultLocale}`)
       logger.info(`${defaultLocaleCode} loaded as default locale.`)
 
       // Load other available locales
       locales
       .filter((file) => file !== defaultLocale)
       .forEach((file) => {
-        const localeCode = file.replace(/.json/, '');
+        const localeCode = file.replace(/.json/, ``)
         const targetLocaleFile = require(`../locales/${file}`)
         //  If structure are similar/same, proceed
         if (this.compareLocaleKeys(this.localesPool[defaultLocaleCode], targetLocaleFile)) {
-          this.localeCodesPool.push(localeCode);
+          this.localeCodesPool.push(localeCode)
           this.localesPool[localeCode] = targetLocaleFile
           logger.info(`${localeCode} registered.`)
         }
         else {
           logger.warn(`${localeCode} has incomplete localization, will be excluded from the pool.`)
         }
-      });
+      })
     }
 
     /**
@@ -94,7 +94,7 @@ class Localizer {
      * @returns {object} - The pool of available locales.
      */
     getLocalesPool() {
-      return this.localesPool;
+      return this.localesPool
     }
 
     /**
@@ -105,20 +105,20 @@ class Localizer {
      * @return {boolean}
      */
     compareLocaleKeys(obj1, obj2) {
-      const keys1 = Object.keys(obj1);
-      const keys2 = Object.keys(obj2);
+      const keys1 = Object.keys(obj1)
+      const keys2 = Object.keys(obj2)
       if (keys1.length !== keys2.length) {
-        return false;
+        return false
       }
 
       for (let key of keys1) {
         if (!keys2.includes(key)) {
-          return false;
+          return false
         }
         // Check if the values of the keys are objects and recursively compare them
-        if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+        if (typeof obj1[key] === `object` && typeof obj2[key] === `object`) {
           if (!this.compareLocaleKeys(obj1[key], obj2[key])) {
-            return false;
+            return false
           }
         }
       }
