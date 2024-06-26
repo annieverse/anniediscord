d216947b61379d3edef5cae48ff5c81e6694922d
Andrew <thefryingpan2021@gmail.com>
2024-06-25 20:44:38 -0400

    Update guildDelete.js

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,4 +1,5 @@
 module.exports = function guildDelete(client, guild) {
     //  Perform logging to support server
-    client.shard.broadcastEval((c,{g}) => c.channels.cache.has(`1021135246177079326`) ? c.channels.cache.get(`1021135246177079326`).send(`NODE_HAS_LEFT:: ${g.id}@${g.name}`) : c.channels.fetch(`1021135246177079326`).then(channel => channel.send(`NODE_HAS_LEFT:: ${g.id}@${g.name}`)).catch(err=>c.logger.error(err)), {context: {g:guild}})
+    if (!guild.available) return
+    client.shard.broadcastEval((c, { g }) => { if (g.available) { c.channels.cache.has(`1021135246177079326`) ? c.channels.cache.get(`1021135246177079326`).send(`NODE_HAS_LEFT:: ${g.id}@${g.name}`) : c.channels.fetch(`1021135246177079326`).then(channel => channel.send(`NODE_HAS_LEFT:: ${g.id}@${g.name}`)).catch(err => c.logger.error(err)) } }, { context: { g: guild } })
 }
