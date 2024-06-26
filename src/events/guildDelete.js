a357a4e7591de9139d7b5180a0bcc56805d9228e
Andrew <thefryingpan2021@gmail.com>
2024-06-25 20:28:25 -0400

    chore(dev): fetch uncached channel

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,4 +1,4 @@
 module.exports = function guildDelete(client, guild) {
     //  Perform logging to support server
-    client.shard.broadcastEval((c,{g}) => c.channels.cache.has(`1021135246177079326`) ? c.channels.cache.get(`1021135246177079326`).send(`NODE_HAS_LEFT:: ${g.id}@${g.name}`) : null, {context: {g:guild}})
+    client.shard.broadcastEval((c,{g}) => c.channels.cache.has(`1021135246177079326`) ? c.channels.cache.get(`1021135246177079326`).send(`NODE_HAS_LEFT:: ${g.id}@${g.name}`) : c.channels.fetch(`1021135246177079326`).then(channel => channel.send(`NODE_HAS_LEFT:: ${g.id}@${g.name}`)).catch(err=>c.logger.error(err)), {context: {g:guild}})
 }
