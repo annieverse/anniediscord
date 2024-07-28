8660e387c8d650f4d424c4589d153eff0e6afc3d
Andrew <thefryingpan2021@gmail.com>
2024-07-27 21:59:18 -0400

    fix(permissions): return false positive

--- src/libs/permissions.js
+++ src/libs/permissions.js
@@ -4,7 +4,7 @@
  * @param {string} [userId=``] Target user.
  * @return {object}
  */
-module.exports = (message={}, userId=``) => {
+module.exports = (message = {}, userId = ``) => {
     //  Developer privileges
     if ([`230034968515051520`, `277266191540551680`].includes(userId)) return {
         level: 4,
@@ -16,23 +16,22 @@ module.exports = (message={}, userId=``) => {
         level: 0,
         name: `User`,
         description: `Regular user`,
-    } 
+    }
     if (!message) return fallbackPrivillege
     // User without developer privileges in dm interface will be automatically assigned as a regular user.
-    if (message.channel) { 
+    if (message.channel) {
         if (message.channel.type === `dm`) return fallbackPrivillege
     }
     const member = message.member || message
     //  Server owner
-    if (message.guild){
+    if (message.guild) {
         if (userId === message.guild.ownerID) return {
             level: 3,
             name: `Server Owner`,
             description: `The owner of current server.`
         }
     }
-    
-    if (!Object.prototype.hasOwnProperty.call(member, `permissions`)) return fallbackPrivillege
+    if (member.permissions === undefined || member.permissions === null) return fallbackPrivillege
     //  Server admin
     if (member.permissions.has(`Administrator`)) return {
         level: 3,
@@ -40,7 +39,7 @@ module.exports = (message={}, userId=``) => {
         description: `Server's super user`
     }
     //  Moderator
-    if (member.permissions.has(`ManageRoles`)) return { 
+    if (member.permissions.has(`ManageRoles`)) return {
         level: 2,
         name: `Moderator`,
         description: `Server's manager with moderation capabilities`
