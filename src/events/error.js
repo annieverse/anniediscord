020b598ed887c252a7389610c26cdee6a7cadb3d
Andrew <thefryingpan2021@gmail.com>
2025-07-17 19:47:32 -0400

    chore(webhook): dev thread

--- src/events/error.js
+++ src/events/error.js
@@ -7,11 +7,14 @@ module.exports = function error(client, e) {
     // if (!client.dev) return // Should return any errors to support server if they arnt caught by other handlers
     //  Report to support server
     client.logger.error(`Ops, something went wrong > ${e}\n${e.stack}`)
-    client.shard.broadcastEval(errorRelay, { context: { fileName: `error.js`, errorType: `normal`,error_message: e.message, error_stack: e.stack, levelZeroErrors:levelZeroErrors } }).catch(err => client.logger.error(`Unable to send message to channel > ${err}`)) 
+    errorRelay(client, { fileName: `error.js`, errorType: `normal`, error_message: e.message, error_stack: e.stack, levelZeroErrors: levelZeroErrors }).catch(err => client.logger.error(`Unable to send message to channel > ${err}`))
+    // client.shard.broadcastEval(errorRelay, { context: { fileName: `error.js`, errorType: `normal`,error_message: e.message, error_stack: e.stack, levelZeroErrors:levelZeroErrors } }).catch(err => client.logger.error(`Unable to send message to channel > ${err}`)) 
     //  When dev err wh is available
     if (wh) {
         wh.send({
             content: `**${new Date()}**\n\`\`\`js\n${e.message}\n${e.stack}\n\`\`\``,
+            username: `Dev Error(s)`,
+            threadId: `1395552222405988362`
         })
     }
 }
