c2086cc18dc25a07006f71783f21609affaec9e1
Andrew <thefryingpan2021@gmail.com>
2025-07-23 17:09:53 -0400

    chore(setwelcomer): update logic
    checks if the content is a image and if its not ignore it and let the user know to try again

--- src/commands/setting/setWelcomer.js
+++ src/commands/setting/setWelcomer.js
@@ -252,7 +252,15 @@ module.exports = {
             this.args = [this.action, `reset`]
         }
         if (options.getSubcommandGroup() === `image`) {
+            // validFileTypes: [`JPEG`,`PNG`,`GIF`,`SVG`,`TIFF`,`BMP`],
+            await interaction.deferReply()
             if (options.getSubcommand() === `attachment`) {
+                const mediaType = options.getAttachment(`set`).contentType
+                if (mediaType && !mediaType.startsWith(`image/`)) return await reply.send(locale.SETWELCOMER.IMAGE_INVALID_UPLOAD, {
+                    socket: {
+                        emoji: await client.getEmoji(`692428969667985458`)
+                    }
+                })
                 this.action = `image`
                 this.args = [this.action, options.getAttachment(`set`).url]
             } else if (options.getSubcommand() === `url`) {
@@ -758,9 +766,25 @@ module.exports = {
                 emoji: await client.getEmoji(`692428969667985458`)
             }
         })
-        const id = uuidv4()
-        const response = await superagent.get(url)
-        const buffer = response.body
+        let id, response, buffer, mediaType
+        try {
+            id = uuidv4()
+            response = await superagent.get(url)
+            buffer = response.body
+            mediaType = response.headers[`content-type`]
+
+        } catch (error) {
+            return await reply.send(locale.SETWELCOMER.IMAGE_INVALID_UPLOAD, {
+                socket: {
+                    emoji: await client.getEmoji(`692428969667985458`)
+                }
+            })
+        }
+        if (mediaType && !mediaType.startsWith(`image/`)) return await reply.send(locale.SETWELCOMER.IMAGE_INVALID_UPLOAD, {
+            socket: {
+                emoji: await client.getEmoji(`692428969667985458`)
+            }
+        })
         try {
             await fs.writeFileSync(`./src/assets/customWelcomer/${id}.png`, buffer)
         } catch (error) {
@@ -900,7 +924,11 @@ module.exports = {
                 url: imageArgs.startsWith(`http`) && imageArgs.length >= 15 ? this.args[1] : null
             }
         }
-        const hasAttachment = message.attachments.first() ? true : false
+        let hasAttachment = message.attachments.first() ? true : false
+        if (hasAttachment) {
+            const mediaType = message.attachments.first().contentType
+            if (mediaType && !mediaType.startsWith(`image/`)) hasAttachment = false
+        }
         const imageArgs = this.args.slice(1).join(` `)
         const hasImageURL = imageArgs.startsWith(`http`) && imageArgs.length >= 15 ? true : false
         return {
