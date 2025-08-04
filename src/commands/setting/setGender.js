ad028110ae66fae5860308d668887fbc0412babe
Andrew <thefryingpan2021@gmail.com>
2025-08-04 17:39:30 -0400

    fix(setgender): invalid locale string

--- src/commands/setting/setGender.js
+++ src/commands/setting/setGender.js
@@ -25,53 +25,27 @@ module.exports = {
     }],
     type: ApplicationCommandType.ChatInput,
     async execute(client, reply, message, arg, locale, prefix) {
-        const userGender = await client.db.userUtils.getUserGender(message.author.id)
-        if (!arg) return await reply.send(locale(`SETGENDER.GUIDE`), {
-            image: `banner_setgender`,
-            socket: {
-                prefix: prefix,
-                currentGender: reply.socketing(locale(`SETGENDER[{${!userGender ? `HASNT_SET` : `ALREADY_SET`}]`), {
-                    gender: userGender ? (userGender.gender === `m` ? `male` : `female`) : null
-                })
-            }
-        })
-        const malePool = [`male`, `ml`, `m`, `boy`, `man`]
-        const femalePool = [`female`, `fl`, `f`, `girl`, `woman`]
-        //  Handle out of range options
-        const key = arg.toLowerCase()
-        const targetGender = malePool.includes(key) ? `m` :
-            femalePool.includes(key) ? `f` : null
-        //  Update/register gender
-        !targetGender ? client.db.userUtils.updateUserGenderToneutral(message.author.id) :
-            client.db.userUtils.updateUserGender(message.author.id, targetGender)
-        return await reply.send(locale(`SETGENDER.SUCCESSFUL`), {
-            status: `success`,
-            socket: {
-                emoji: await client.getEmoji(`789212493096026143`)
-            }
-        })
+        return await this.run(client, reply, message, arg, locale)
     },
     async Iexecute(client, reply, interaction, options, locale) {
-        const userGender = await client.db.userUtils.getUserGender(interaction.member.id)
         const key = options.getString(`gender`)
+        return await this.run(client, reply, interaction, key, locale)
+    },
+    async run(client, reply, messageRef, key, locale) {
+        const userGender = await client.db.userUtils.getUserGender(messageRef.member.id)
         if (!key) return await reply.send(locale(`SETGENDER.GUIDE`), {
             image: `banner_setgender`,
             socket: {
                 prefix: `/`,
-                currentGender: reply.socketing(locale(`SETGENDER[${!userGender ? `HASNT_SET` : `ALREADY_SET`}]`), {
+                currentGender: reply.socketing(locale(`SETGENDER.${!userGender ? `HASNT_SET` : `ALREADY_SET`}`), {
                     gender: userGender ? (userGender.gender === `m` ? `male` : `female`) : null
                 })
             }
         })
         const malePool = [`male`, `ml`, `m`, `boy`, `man`]
         const femalePool = [`female`, `fl`, `f`, `girl`, `woman`]
-        //  Handle out of range options
-        const targetGender = malePool.includes(key.toLowerCase()) ? `m` :
-            femalePool.includes(key.toLowerCase()) ? `f` :
-                null
-        !targetGender ? client.db.userUtils.updateUserGenderToneutral(interaction.member.id) :
-            //  Update/register gender
-            client.db.userUtils.updateUserGender(interaction.member.id, targetGender)
+        const targetGender = malePool.includes(key.toLowerCase()) ? `m` : femalePool.includes(key.toLowerCase()) ? `f` : null
+        !targetGender ? client.db.userUtils.updateUserGenderToneutral(messageRef.member.id) : /* Update/register gender */ client.db.userUtils.updateUserGender(messageRef.member.id, targetGender)
         return await reply.send(locale(`SETGENDER.SUCCESSFUL`), {
             status: `success`,
             socket: {
