e0df178cb78c17eea10ab4bb644aed3e3938bd9a
Andrew <thefryingpan2021@gmail.com>
2025-08-04 18:19:09 -0400

    chore(locale): updated test

--- tests/libs/localizer.js
+++ tests/libs/localizer.js
@@ -79,9 +79,24 @@ describe(`Localizer Library`, () => {
       localizer.lang = `fr`
       localizer.findLocale(`THIS.KEY._DOES_NOT_EXIST`)
       expect(consoleErrorSpy.calledOnce).to.be.true
-      expect(consoleErrorSpy.args[0][0]).to.equal(
-        `The specified key is not an available language path.\nKey supplied and tried > THIS.KEY._DOES_NOT_EXIST`
-      )
+      /**
+       * Explanation of the regex:
+       * ^: Matches the beginning of the string.
+       * (Mon|Tue|Wed|Thu|Fri|Sat|Sun): Matches the three-letter abbreviation for the day of the week.
+       * \s: Matches a single whitespace character.
+       * (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec): Matches the three-letter abbreviation for the month.
+       * \s: Matches a single whitespace character.
+       * (\d{2}): Matches the two-digit day of the month.
+       * \s: Matches a single whitespace character.
+       * (\d{4}): Matches the four-digit year.
+       * \s: Matches a single whitespace character.
+       * (\d{2}:\d{2}:\d{2}): Matches the time in HH:MM:SS format.
+       * \sGMT[+-]\d{4}: Matches " GMT" followed by a plus or minus sign and a four-digit offset (e.g., "+0500").
+       * \s\(.+\): Matches a space, an opening parenthesis, any character (except newline) one or more times, and a closing parenthesis, representing the timezone name (e.g., "(Coordinated Universal Time)").
+       */
+      const dateRegex = /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s(\d{2})\s(\d{4})\s(\d{2}:\d{2}:\d{2})\sGMT[+-]\d{4}\s\(.+\)/
+      const expectedMessage = ` | The specified key is not an available language path.\nKey supplied and tried > THIS.KEY._DOES_NOT_EXIST`
+      expect(consoleErrorSpy.args[0][0]).to.match(dateRegex).and.to.include(expectedMessage)
     })
 
     it(`should fallback to 'en' version when key not found in other language`, () => {
