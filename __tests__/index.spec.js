2661758211e4891103689097ffce160fa38fa515
Nabil <nightcore.raven07@gmail.com>
2025-08-09 02:55:34 +0700

    test: stateless without global mutation.

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -3,11 +3,18 @@ const { getMessageMock, getClientMock } = require("../__mocks__/index.js");
 
 describe("Message Handler", () => {
 
-    const message = getMessageMock()
-    const client = getClientMock()
+    let message
+    let client
 
+    /**
+     * Make the parameters state reset once on every test scope.
+     * This way we avoid accidental global mutation by always redefining the state.
+     * Stateless and stays within local-scope.
+     */
     beforeEach(() => {
         jest.clearAllMocks();
+        message = getMessageMock();
+        client = getClientMock();
     });
 
     it("it should return if client isn't ready", async () => {
@@ -19,12 +26,15 @@ describe("Message Handler", () => {
     });
 
     it("should return when a bot sends a message", async () => {
+        client.isReady.mockReturnValue(true);
         message.author.bot = true;
         await messageHandler(client, message);
         expect(message.channel.send).not.toHaveBeenCalled();
     });
 
     it("should return when the message is from a DM channel", async () => {
+        client.isReady.mockReturnValue(true);
+        message.author.bot = true;
         message.channel.isDMBased.mockReturnValue(true);
         await messageHandler(client, message);
         console.log(message.channel.isDMBased())
